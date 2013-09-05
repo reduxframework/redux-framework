6b98a4f2512b0f1463c61e3c840365c8103deed5
Ghost1227 <dgriffiths@ghost1227.com>
2013-09-05 18:50:30 +0000

    Code standardization

--- inc/fields/button_set/field_button_set.php
+++ inc/fields/button_set/field_button_set.php
@@ -26,87 +26,87 @@ if( !defined( 'ABSPATH' ) ) exit;
 // Don't duplicate me!
 if( !class_exists( 'ReduxFramework_button_set' ) ) {
 
- 	/**
-     * Main ReduxFramework_button_set class
+    /**
+     * Main ReduxFramework_border class
      *
      * @since       1.0.0
      */
-	class ReduxFramework_button_set extends ReduxFramework{	
-	
-		/**
-	 	 * Field Constructor.
-	 	 *
-	 	 * Required - must call the parent constructor, then assign field and value to vars, and obviously call the render field function
-	 	 *
-	 	 * @since 		1.0.0
-	 	 * @access		public
-	 	 * @return		void
-		 */
-		public function __construct( $field = array(), $value ='', $parent ) {
-		
-			parent::__construct( $parent->sections, $parent->args, $parent->extra_tabs );
+    class ReduxFramework_button_set extends ReduxFramework{ 
+    
+        /**
+         * Field Constructor.
+         *
+         * Required - must call the parent constructor, then assign field and value to vars, and obviously call the render field function
+         *
+         * @since       1.0.0
+         * @access      public
+         * @return      void
+         */
+        public function __construct( $field = array(), $value ='', $parent ) {
+        
+            parent::__construct( $parent->sections, $parent->args, $parent->extra_tabs );
 
-			$this->field = $field;
-			$this->value = $value;
-		
-		}
-	
-	
-		/**
-		 * Field Render Function.
-	 	 *
-	  	 * Takes the vars and outputs the HTML for the field in the settings
-		 *
-		 * @since 		1.0.0
-		 * @access		public
-		 * @return		void
-		 */
-		public function render() {
-		
-			$class = ( isset( $this->field['class'] ) ) ? ' ' . $this->field['class'] . '" ' : '';
-		
-			if( !empty( $this->field['compiler'] ) && $this->field['compiler'] )
-				$class .= " compiler";
-		
-			echo '<fieldset class="buttonset">';
-			
-			foreach( $this->field['options'] as $k => $v ) {
-				
-				echo '<input type="radio" id="' . $this->field['id'] . '_' . array_search( $k, array_keys( $this->field['options'] ) ) . '" name="' . $this->args['opt_name'] . '[' . $this->field['id'] . ']" class="' . $class . '" value="' . $k . '" ' . checked( $this->value, $k, false ) . '/>';
-				echo '<label for="' . $this->field['id'] . '_' . array_search( $k, array_keys($this->field['options'] ) ) . '">' . $v . '</label>';
-				
-			}
-			
-			echo ( isset( $this->field['desc'] ) && !empty( $this->field['desc'] ) ) ? '<div class="description">' . $this->field['desc'] . '</div>' : '';
-		
-			echo '</fieldset>';
-		
-		}
-	
-		
-		/**
-		 * Enqueue Function.
-		 *
-		 * If this field requires any scripts, or css define this function and register/enqueue the scripts/css
-		 *
-	 	 * @since 		1.0.0
-	 	 * @access		public
-	 	 * @return		void
-		 */
-		public function enqueue() {
-		
-			wp_enqueue_style( 'jquery-ui-css' );
+            $this->field = $field;
+            $this->value = $value;
+        
+        }
+    
+    
+        /**
+         * Field Render Function.
+         *
+         * Takes the vars and outputs the HTML for the field in the settings
+         *
+         * @since       1.0.0
+         * @access      public
+         * @return      void
+         */
+        public function render() {
+        
+            $class = ( isset( $this->field['class'] ) ) ? ' ' . $this->field['class'] . '" ' : '';
+        
+            if( !empty( $this->field['compiler'] ) && $this->field['compiler'] )
+                $class .= " compiler";
+        
+            echo '<fieldset class="buttonset">';
+            
+            foreach( $this->field['options'] as $k => $v ) {
+                
+                echo '<input type="radio" id="' . $this->field['id'] . '_' . array_search( $k, array_keys( $this->field['options'] ) ) . '" name="' . $this->args['opt_name'] . '[' . $this->field['id'] . ']" class="' . $class . '" value="' . $k . '" ' . checked( $this->value, $k, false ) . '/>';
+                echo '<label for="' . $this->field['id'] . '_' . array_search( $k, array_keys($this->field['options'] ) ) . '">' . $v . '</label>';
+                
+            }
+            
+            echo ( isset( $this->field['desc'] ) && !empty( $this->field['desc'] ) ) ? '<div class="description">' . $this->field['desc'] . '</div>' : '';
+        
+            echo '</fieldset>';
+        
+        }
+    
+        
+        /**
+         * Enqueue Function.
+         *
+         * If this field requires any scripts, or css define this function and register/enqueue the scripts/css
+         *
+         * @since       1.0.0
+         * @access      public
+         * @return      void
+         */
+        public function enqueue() {
+        
+            wp_enqueue_style( 'jquery-ui-css' );
 
-			wp_enqueue_script(
-				'redux-field-button_set-js', 
-				REDUX_URL . 'inc/fields/button_set/field_button_set.min.js', 
-				array( 'jquery', 'jquery-ui-core', 'jquery-ui-dialog' ),
-				time(),
-				true
-			);
+            wp_enqueue_script(
+                'redux-field-button_set-js', 
+                REDUX_URL . 'inc/fields/button_set/field_button_set.min.js', 
+                array( 'jquery', 'jquery-ui-core', 'jquery-ui-dialog' ),
+                time(),
+                true
+            );
 
-		}
-	
-	}
+        }
+    
+    }
 }
 ?>
