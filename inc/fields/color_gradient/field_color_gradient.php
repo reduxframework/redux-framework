307841e284b4f32fbd501bd450a996cfcc1079da
Ghost1227 <dgriffiths@ghost1227.com>
2013-09-05 19:05:54 +0000

    Standardized code

--- inc/fields/color_gradient/field_color_gradient.php
+++ inc/fields/color_gradient/field_color_gradient.php
@@ -1,83 +1,119 @@
 <?php
-class ReduxFramework_color_gradient extends ReduxFramework{	
-	
-	/**
-	 * Field Constructor.
-	 *
-	 * Required - must call the parent constructor, then assign field and value to vars, and obviously call the render field function
-	 *
-	 * @since ReduxFramework 1.0.0
-	*/
-	function __construct($field = array(), $value ='', $parent){
-		
-		parent::__construct($parent->sections, $parent->args, $parent->extra_tabs);
-		$this->field = $field;
-		$this->value = $value;
-		//$this->render();
-		
-	}//function
-	
-	
-	
-	/**
-	 * Field Render Function.
-	 *
-	 * Takes the vars and outputs the HTML for the field in the settings
-	 *
-	 * @since ReduxFramework 1.0.0
-	*/
-	function render(){
-		
-		// No errors please
-		$defaults = array(
-			'from' => '',
-			'to' => ''
-			);
-		$this->value = wp_parse_args( $this->value, $defaults );
+/**
+ * Redux Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Redux Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Redux Framework. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package     ReduxFramework
+ * @subpackage  Field_Color_Gradient
+ * @author      Daniel J Griffiths (Ghost1227)
+ * @author      Dovy Paukstys
+ * @version     3.0.0
+ */
 
-		$class = (isset($this->field['class']))?' '.$this->field['class'].'" ':'';
-		if (!empty($this->field['compiler']) && $this->field['compiler']) {
-			$class .= " compiler";
-		}
+// Exit if accessed directly
+if( !defined( 'ABSPATH' ) ) exit;
 
-		echo '<div class="redux-color-gradient-container" id="'.$this->field['id'].'">';
+// Don't duplicate me!
+if( !class_exists( 'ReduxFramework_color_gradient' ) ) {
 
-		echo '<strong>' . __('From ', 'redux') . '</strong>&nbsp;<input id="'.$this->field['id'].'-from" name="'.$this->args['opt_name'].'['.$this->field['id'].'][from]" value="'.$this->value['from'].'" class="redux-color redux-color-init ' . $class . '"  type="text" value="' . $this->value . '"  data-default-color="' . $this->field['std']['from'] . '" />';
+    /**
+     * Main ReduxFramework_color_gradient class
+     *
+     * @since       1.0.0
+     */
+    class ReduxFramework_color_gradient extends ReduxFramework {
+    
+        /**
+         * Field Constructor.
+         *
+         * Required - must call the parent constructor, then assign field and value to vars, and obviously call the render field function
+         *
+         * @since       1.0.0
+         * @access      public
+         * @return      void
+         */
+        public function __construct( $field = array(), $value ='', $parent ) {
+        
+            parent::__construct( $parent->sections, $parent->args, $parent->extra_tabs );
 
-		echo '&nbsp;&nbsp;&nbsp;&nbsp;<strong>' . __('To ', 'redux') . '</strong>&nbsp;<input id="'.$this->field['id'].'-to" name="'.$this->args['opt_name'].'['.$this->field['id'].'][to]" value="'.$this->value['to'].'" class="redux-color redux-color-init ' . $class . '"  type="text" value="' . $this->value . '"  data-default-color="' . $this->field['std']['to'] . '" />';
-		
-		echo (isset($this->field['desc']) && !empty($this->field['desc']))?'<div class="description">'.$this->field['desc'].'</div>':'';
-		
-		echo '</div>';
-		
-	}//function
-	
-	
-	/**
-	 * Enqueue Function.
-	 *
-	 * If this field requires any scripts, or css define this function and register/enqueue the scripts/css
-	 *
-	 * @since ReduxFramework 1.0.0
-	*/
-	function enqueue(){
-		
-		wp_enqueue_script(
-			'redux-field-color-js', 
-			REDUX_URL.'inc/fields/color/field_color.js', 
-			array('jquery', 'wp-color-picker'),
-			time(),
-			true
-		);
+            $this->field = $field;
+            $this->value = $value;
+        
+        }
 
-		wp_enqueue_style(
-			'redux-field-color-js', 
-			REDUX_URL.'inc/fields/color/field_color.css', 
-			time(),
-			true
-		);		
-		
-	}//function
-	
-}//class
+        /**
+         * Field Render Function.
+         *
+         * Takes the vars and outputs the HTML for the field in the settings
+         *
+         * @since       1.0.0
+         * @access      public
+         * @return      void
+         */
+        public function render() {
+        
+            // No errors please
+            $defaults = array(
+                'from' => '',
+                'to' => ''
+            );
+
+            $this->value = wp_parse_args( $this->value, $defaults );
+
+            $class = ( isset( $this->field['class'] ) ) ? ' ' . $this->field['class'] . '" ' : '';
+        
+            if( !empty( $this->field['compiler'] ) && $this->field['compiler'] )
+                $class .= " compiler";
+
+            echo '<div class="redux-color-gradient-container" id="' . $this->field['id'] . '">';
+
+            echo '<strong>' . __( 'From ', 'redux-framework' ) . '</strong>&nbsp;<input id="' . $this->field['id'] . '-from" name="' . $this->args['opt_name'] . '[' . $this->field['id'] . '][from]" value="'.$this->value['from'].'" class="redux-color redux-color-init ' . $class . '"  type="text" value="' . $this->value . '"  data-default-color="' . $this->field['std']['from'] . '" />';
+
+            echo '&nbsp;&nbsp;&nbsp;&nbsp;<strong>' . __( 'To ', 'redux-framework' ) . '</strong>&nbsp;<input id="' . $this->field['id'] . '-to" name="' . $this->args['opt_name'] . '[' . $this->field['id'] . '][to]" value="' . $this->value['to'] . '" class="redux-color redux-color-init ' . $class . '"  type="text" value="' . $this->value . '"  data-default-color="' . $this->field['std']['to'] . '" />';
+        
+            echo ( isset( $this->field['desc'] ) && !empty( $this->field['desc'] ) ) ? '<div class="description">' . $this->field['desc'] . '</div>' : '';
+        
+            echo '</div>';
+        
+        }
+    
+        /**
+         * Enqueue Function.
+         *
+         * If this field requires any scripts, or css define this function and register/enqueue the scripts/css
+         *
+         * @since       1.0.0
+         * @access      public
+         * @return      void
+         */
+        public function enqueue() {
+        
+            wp_enqueue_script(
+                'redux-field-color-js', 
+                REDUX_URL . 'inc/fields/color/field_color.js', 
+                array( 'jquery', 'wp-color-picker' ),
+                time(),
+                true
+            );
+
+            wp_enqueue_style(
+                'redux-field-color-js', 
+                REDUX_URL . 'inc/fields/color/field_color.css', 
+                time(),
+                true
+            );      
+        
+        }
+    }
+}
 ?>
