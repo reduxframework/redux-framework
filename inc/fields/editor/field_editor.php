712c3fef28248be84ff46623c292f7b52e7a3204
Ghost1227 <dgriffiths@ghost1227.com>
2013-09-05 20:03:14 +0000

    Standardized code

--- inc/fields/editor/field_editor.php
+++ inc/fields/editor/field_editor.php
@@ -1,48 +1,82 @@
 <?php
-class ReduxFramework_editor extends ReduxFramework{	
-	
-	/**
-	 * Field Constructor.
-	 *
-	 * Required - must call the parent constructor, then assign field and value to vars, and obviously call the render field function
-	 *
-	 * @since ReduxFramework 1.0.0
-	*/
-	function __construct($field = array(), $value ='', $parent){
-		
-		parent::__construct($parent->sections, $parent->args, $parent->extra_tabs);
-		$this->field = $field;
-		$this->value = $value;
-		//$this->render();
-		
-	}//function
-	
-	
-	
-	/**
-	 * Field Render Function.
-	 *
-	 * Takes the vars and outputs the HTML for the field in the settings
-	 *
-	 * @since ReduxFramework 1.0.0
-	*/
-	function render(){
-		
-		$class = (isset($this->field['class']))?' '.$this->field['class'].'" ':'';
-		if (!empty($this->field['compiler']) && $this->field['compiler']) {
-			$class .= " compiler";
-		}
-		
-		//echo '<textarea id="'.$this->field['id'].'" name="'.$this->args['opt_name'].'['.$this->field['id'].']" class="'.$class.'" rows="6" >'.$this->value.'</textarea>';
-		$settings = array(
-			'textarea_name' => $this->args['opt_name'].'['.$this->field['id'].']', 
-			'editor_class' => $class
-			);
-		wp_editor($this->value, $this->field['id'], $settings );
-		
-		echo (isset($this->field['desc']) && !empty($this->field['desc']))?'<div class="description">'.$this->field['desc'].'</div>':'';
-		
-	}//function
-	
-}//class
+/**
+ * Redux Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Redux Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Redux Framework. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package     ReduxFramework
+ * @subpackage  Field_Editor
+ * @author      Daniel J Griffiths (Ghost1227)
+ * @author      Dovy Paukstys
+ * @version     3.0.0
+ */
+
+// Exit if accessed directly
+if( !defined( 'ABSPATH' ) ) exit;
+
+// Don't duplicate me!
+if( !class_exists( 'ReduxFramework_editor' ) ) {
+
+    /**
+     * Main ReduxFramework_editor class
+     *
+     * @since       1.0.0
+     */
+    class ReduxFramework_editor extends ReduxFramework {
+    
+        /**
+         * Field Constructor.
+         *
+         * Required - must call the parent constructor, then assign field and value to vars, and obviously call the render field function
+         *
+         * @since       1.0.0
+         * @access      public
+         * @return      void
+         */
+        public function __construct( $field = array(), $value ='', $parent ) {
+        
+            parent::__construct( $parent->sections, $parent->args, $parent->extra_tabs );
+
+            $this->field = $field;
+            $this->value = $value;
+        
+        }
+
+        /**
+         * Field Render Function.
+         *
+         * Takes the vars and outputs the HTML for the field in the settings
+         *
+         * @since       1.0.0
+         * @access      public
+         * @return      void
+         */
+        public function render() {
+        
+            $class = ( isset( $this->field['class'] ) ) ? ' ' . $this->field['class'] . '" ' : '';
+
+            if( !empty( $this->field['compiler'] ) && $this->field['compiler'] )
+                $class .= ' compiler';
+        
+            $settings = array(
+                'textarea_name' => $this->args['opt_name'] . '[' . $this->field['id'] . ']', 
+                'editor_class'  => $class
+            );
+
+            wp_editor( $this->value, $this->field['id'], $settings );
+        
+            echo ( isset( $this->field['desc'] ) && !empty( $this->field['desc'] ) ) ? '<div class="description">' . $this->field['desc'] . '</div>' : '';
+        
+        }
+    }
+}
 ?>
