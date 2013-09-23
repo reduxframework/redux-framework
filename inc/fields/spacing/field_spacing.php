a0298893247fd5b7fa85051f7069dd58840f4292
Dovy <info@simplerain.com>
2013-09-24 00:04:11 +0200

    Missing comma caused error

--- inc/fields/spacing/field_spacing.php
+++ inc/fields/spacing/field_spacing.php
@@ -42,7 +42,7 @@ class ReduxFramework_spacing extends ReduxFramework{
 		$defaults = array(
 			'top'=>'',
 			'right'=>'',
-			'mode' => ''
+			'mode' => '',
 			'bottom'=>'',
 			'left'=>'',
 			'units'=>'px',
