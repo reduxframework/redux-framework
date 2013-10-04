1518e922472c99297e6d9f50eb5a172575377698
Dovy <info@simplerain.com>
2013-10-04 23:14:26 +0200

    Small comment to know to look into something

--- ReduxCore/framework.php
+++ ReduxCore/framework.php
@@ -977,6 +977,7 @@ if( !class_exists( 'ReduxFramework' ) ) {
 							$default_output = "";
 						    if (!is_array($field['default'])) {
 								if ( !empty( $field['options'][$field['default']] ) ) {
+									// TODO: This serialize fix may not be the best solution. Look into it. PHP 5.4 error without serialize
 									$default_output .= serialize($field['options'][$field['default']]).", ";
 								} else if ( !empty( $field['options'][$field['default']] ) ) {
 									$default_output .= $field['options'][$field['default']].", ";
