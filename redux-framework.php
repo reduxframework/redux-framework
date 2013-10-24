26f89785edb79743a5bd4c3ad6a01499721c062f
Dovy <info@simplerain.com>
2013-10-24 16:08:53 +0200

    Add the actual plugin URL.

--- redux-framework.php
+++ redux-framework.php
@@ -13,7 +13,7 @@
  *
  * @wordpress-plugin
  * Plugin Name: Redux Framework
- * Plugin URI:  http://reduxframework.com
+ * Plugin URI:  http://wordpress.org/plugins/redux-framework/
  * Github URI:  https://github.com/ReduxFramework/ReduxFramework
  * Description: Redux is a simple, truly extensible options framework for WordPress themes and plugins.
  * Version:     3.0.0
