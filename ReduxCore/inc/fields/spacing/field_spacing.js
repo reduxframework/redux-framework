028ef1b26b16c8ecef48bd1672a7bf519765ddf8
Dovy <info@simplerain.com>
2013-10-27 15:24:48 +0100

    Fix for issue #129

--- ReduxCore/inc/fields/spacing/field_spacing.js
+++ ReduxCore/inc/fields/spacing/field_spacing.js
@@ -17,9 +17,12 @@
 		});
 
 		$('.redux-spacing-input').on('change', function() {
-			var units = $(this).parents('.redux-spacing-container:first').find('.redux-spacing-units option:selected').val();
-			var id = $(this).attr('rel');
-			$('#'+id).val($(this).val()+units);
+			var units = jQuery(this).parents('.redux-field:first').find('.redux-spacing-units option:selected').val();
+			if(typeof units !== 'undefined') {
+				jQuery('#'+jQuery(this).attr('rel')).val(jQuery(this).val()+units);
+			} else {
+				jQuery('#'+jQuery(this).attr('rel')).val(jQuery(this).val());
+			}
 		});
 
 		$('.redux-spacing-units').on('change', function() {
