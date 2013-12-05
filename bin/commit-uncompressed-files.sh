784cf82b422a755072dcb3a7b1b8c230723a272c
Dovy Paukstys <info@simplerain.com>
2013-12-05 05:27:58 +0100

    Another test

--- bin/commit-uncompressed-files.sh
+++ bin/commit-uncompressed-files.sh
@@ -3,13 +3,12 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_JOB_NUMBER" == *.1 ]]; then
   	echo -e "Checking to make sure files are properly compressed.\n"
 
 	v1=`find ReduxCore -type f | sort -u | xargs cat | md5sum`
-
+	echo "$v1"
 	grunt compileCSS
 	grunt compileJS
-	
 	v2=`find ReduxCore -type f | sort -u | xargs cat | md5sum`
-
-	if [ "$v1" == "$v1" ]
+	echo "$v2"
+	if [ "$v1" -eq "$v1" ]
 	then
 	    echo "All files are properly compressed."
 	else
