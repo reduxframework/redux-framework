1320a45d61ccee66386e18d95b36a9fe5b93d60f
Dovy Paukstys <info@simplerain.com>
2013-12-05 05:17:30 +0100

    Test the compile back feature.

--- bin/commit-uncompressed-files.sh
+++ bin/commit-uncompressed-files.sh
@@ -2,8 +2,6 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_JOB_NUMBER" == *.1 ]]; then
 	
   	echo -e "Checking to make sure files are properly compressed.\n"
 
-  	`find ReduxCore -type f | sort -u | xargs cat | md5sum > tmp/v1`
-
 	v1=`find ReduxCore -type f | sort -u | xargs cat | md5sum`
 
 	grunt compileCSS
