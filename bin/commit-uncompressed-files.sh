e4aa3741d9d6d97d35346a78f37441e5eff0ce71
Dovy Paukstys <info@simplerain.com>
2013-12-05 07:54:39 +0100

    7

--- bin/commit-uncompressed-files.sh
+++ bin/commit-uncompressed-files.sh
@@ -8,7 +8,7 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_JOB_NUMBER" == *.1 ]]; then
 	grunt compileJS
 	v2=`find ReduxCore -type f | sort -u | xargs cat | md5sum`
 	echo "$v2"
-	if [ "$v1" == "$v2" ] then
+	if [ "$v1" == "$v2" ]; then
 	    echo "All files are properly compressed."
 	else
 		echo "Files are not the same. Committing back to the repo."
