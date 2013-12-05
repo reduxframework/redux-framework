72a757e003d3e37fb6b7665c71e290d4b8f24e5c
Dovy Paukstys <info@simplerain.com>
2013-12-05 05:01:26 +0100

    Another travis fix. This should be good.

---
+++ bin/commit-uncompressed-files.sh
@@ -0,0 +1,22 @@
+if [ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_JOB_NUMBER" == *.1 ]; then
+	
+  	echo -e "Checking to make sure files are properly compressed.\n"
+
+  	`find ReduxCore -type f | sort -u | xargs cat | md5sum > tmp/v1`
+
+	v1=`find ReduxCore -type f | sort -u | xargs cat | md5sum`
+
+	grunt compressCSS
+	grunt compressJS
+	
+	v2=`find ReduxCore -type f | sort -u | xargs cat | md5sum`
+
+	if [ "$value" == "$value2" ]
+	then
+	    echo "All files are properly compressed."
+	else
+		echo "Files are not the same. Committing back to the repo."
+		grunt gh-pages:compressedFiles
+	fi
+
+fi
