0c72ab085af1662be2247314826694ff67b75602
Dovy Paukstys <info@simplerain.com>
2013-12-06 02:21:30 +0100

    Fix for issue #275

--- ReduxCore/inc/fields/sortable/field_sortable.php
+++ ReduxCore/inc/fields/sortable/field_sortable.php
@@ -11,7 +11,7 @@ class ReduxFramework_sortable {
     function __construct($field = array(), $value ='', $parent) {
         $this->field = $field;
         $this->value = $value;
-        $this->args = $parent->args;
+        $this->parent = $parent;
     }
 
     /**
@@ -48,17 +48,17 @@ class ReduxFramework_sortable {
             }
         }
 
-
         echo '<ul id="'.$this->field['id'].'-list" class="redux-sortable ' . $class . '">';
 
         foreach ($this->value as $k => $nicename) {
-            $value_display = isset($this->value[$k]) ? $this->value[$k] : '';
+            
             echo '<li>';
             
             $checked = "";
-            $name = $this->args['opt_name'] . '[' . $this->field['id'] . '][' . $k . ']';
+            $name = $this->parent->args['opt_name'] . '[' . $this->field['id'] . '][' . $k . ']';
 
             if ( $this->field['mode'] == "checkbox") {
+            	$value_display = $this->field['options'][$k];
                 if (!empty($this->value[$k])) {
                     $checked = 'checked="checked" ';
                 }
@@ -66,9 +66,9 @@ class ReduxFramework_sortable {
 
                 echo '<input type="hidden" name="'.$name.'" id="'.$this->field['id'].'-'.$k.'-hidden" value="'.$value_display.'" />';
                 $name = "";
-            }
-            if ( $this->field['mode'] == "checkbox") {
                 echo '<div class="checkbox-container">';
+            } else {
+            	$value_display = isset($this->value[$k]) ? $this->value[$k] : '';
             }
             echo '<input rel="'.$this->field['id'].'-'.$k.'-hidden" class="' . $class . '" '.$checked.'type="'.$this->field['mode'].'" id="' . $this->field['id'] . '[' . $k . ']" name="'.$name.'" value="' . esc_attr($value_display) . '" placeholder="' . $nicename . '" />';
 
