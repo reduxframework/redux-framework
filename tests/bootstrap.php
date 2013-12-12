399f468bb0bd2bf8eae84baea27d8f81426fe008
Sandro Bilbeisi <sandro.bilbeisi@gmail.com>
2013-12-12 05:12:08 +0100

    manual sync to 3.1.2

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,14 @@
 <?php
 
-ini_set('display_errors','on');
-error_reporting(E_ALL);
+$_tests_dir = getenv('WP_TESTS_DIR');
+if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
-require_once dirname( __FILE__ ) . '/../tmp/wordpress-tests/includes/functions.php';
+require_once $_tests_dir . '/includes/functions.php';
 
-require dirname( __FILE__ ) . '/../tmp/wordpress-tests/includes/bootstrap.php';
+function _manually_load_plugin() {
+	require dirname( __FILE__ ) . '/../redux-framework.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+require $_tests_dir . '/includes/bootstrap.php';
+
