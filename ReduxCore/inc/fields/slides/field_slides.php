34e36cd4e08cd2e90835e5820276b4b28c3dbe41
Ghost1227 <dgriffiths@ghost1227.com>
2013-12-17 05:15:52 +0000

    Fix array to string conversion - issue #404

--- ReduxCore/inc/fields/slides/field_slides.php
+++ ReduxCore/inc/fields/slides/field_slides.php
@@ -163,7 +163,7 @@ if (!class_exists('ReduxFramework_slides')) {
             }
 
             if ($x == 0) {
-                echo '<div class="redux-slides-accordion-group"><fieldset class="redux-field" data-id="'.$this->field.'"><h3><span class="redux-slides-header">New Slide</span></h3><div>';
+                echo '<div class="redux-slides-accordion-group"><fieldset class="redux-field" data-id="'.$this->field['id'].'"><h3><span class="redux-slides-header">New Slide</span></h3><div>';
 
                 $hide = ' hide';
 
