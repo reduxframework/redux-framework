b1ec7bcfa8b098728bdf5e79e9c357318a943776
Kevin Provance <kevin.provance@gmail.com>
2014-01-10 22:11:16 -0500

    Adding build number to version number for dev purposes.

--- ReduxCore/framework.php
+++ ReduxCore/framework.php
@@ -48,7 +48,10 @@ if( !class_exists( 'ReduxFramework' ) ) {
      */
     class ReduxFramework {
 
-        public static $_version = '3.1.4';
+	// ATTENTION DEVS
+	// Please update the build number with each push, no matter how small.
+	// This will make for easier support when we ask users what version they are using.
+        public static $_version = '3.1.4.20';
         public static $_dir;
         public static $_url;
         public static $_properties;
