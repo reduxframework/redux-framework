8e3af45c1f449544f64039b60fc68e440a5844b3
Kevin Provance <kevin.provance@gmail.com>
2014-01-13 20:26:07 -0500

    build increment

--- ReduxCore/framework.php
+++ ReduxCore/framework.php
@@ -51,7 +51,7 @@ if( !class_exists( 'ReduxFramework' ) ) {
 	// ATTENTION DEVS
 	// Please update the build number with each push, no matter how small.
 	// This will make for easier support when we ask users what version they are using.
-        public static $_version = '3.1.4.23';
+        public static $_version = '3.1.4.24';
         public static $_dir;
         public static $_url;
         public static $_properties;
