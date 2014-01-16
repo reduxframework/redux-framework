e827e987ff99b70f4cda21cd5c7d01ca53b24229
Kevin Provance <kevin.provance@gmail.com>
2014-01-15 19:02:13 -0500

    Refactored google code matching to ignore comma spaces

--- ReduxCore/inc/fields/typography/field_typography.php
+++ ReduxCore/inc/fields/typography/field_typography.php
@@ -3,31 +3,26 @@
 
 class ReduxFramework_typography extends ReduxFramework {
 
-    // Please note, if you intend to add fonts to this array,
-    // the font sequence in the key must NOT have spaces after the
-    // comma, or things get broke later on.  Spaces in the value are fine. 
-    // This might be 'fixable' later on with Regex, if the key format becomes
-    // an issue. - kp
     private $std_fonts = array(
-        "Arial,Helvetica,sans-serif"                        => "Arial, Helvetica, sans-serif",
-        "'Arial Black',Gadget,sans-serif"                   => "'Arial Black', Gadget, sans-serif",
-        "'Bookman Old Style',serif"                         => "'Bookman Old Style', serif",
-        "'Comic Sans MS',cursive"                           => "'Comic Sans MS', cursive",
-        "Courier,monospace"                                 => "Courier, monospace",
-        "Garamond,serif"                                    => "Garamond, serif",
-        "Georgia,serif"                                     => "Georgia, serif",
-        "Impact,Charcoal,sans-serif"                        => "Impact, Charcoal, sans-serif",
-        "'Lucida Console',Monaco,monospace"                 => "'Lucida Console', Monaco, monospace",
-        "'Lucida Sans Unicode','Lucida Grande',sans-serif"  => "'Lucida Sans Unicode', 'Lucida Grande', sans-serif",
-        "'MS Sans Serif',Geneva,sans-serif"                 => "'MS Sans Serif', Geneva, sans-serif",
-        "'MS Serif','New York',sans-serif"                  => "'MS Serif', 'New York', sans-serif",
-        "'Palatino Linotype','Book Antiqua',Palatino,serif" => "'Palatino Linotype', 'Book Antiqua', Palatino, serif",
-        "Tahoma,Geneva,sans-serif"                          => "Tahoma, Geneva, sans-serif",
-        "'Times New Roman',Times,serif"                     => "'Times New Roman', Times, serif",
-        "'Trebuchet MS',Helvetica,sans-serif"               => "'Trebuchet MS', Helvetica, sans-serif",
-        "Verdana,Geneva,sans-serif"                         => "Verdana, Geneva, sans-serif",    
+        "Arial, Helvetica, sans-serif"                          => "Arial, Helvetica, sans-serif",
+        "'Arial Black', Gadget, sans-serif"                     => "'Arial Black', Gadget, sans-serif",
+        "'Bookman Old Style', serif"                            => "'Bookman Old Style', serif",
+        "'Comic Sans MS', cursive"                              => "'Comic Sans MS', cursive",
+        "Courier, monospace"                                    => "Courier, monospace",
+        "Garamond, serif"                                       => "Garamond, serif",
+        "Georgia, serif"                                        => "Georgia, serif",
+        "Impact, Charcoal, sans-serif"                          => "Impact, Charcoal, sans-serif",
+        "'Lucida Console', Monaco, monospace"                   => "'Lucida Console', Monaco, monospace",
+        "'Lucida Sans Unicode', 'Lucida Grande', sans-serif"    => "'Lucida Sans Unicode', 'Lucida Grande', sans-serif",
+        "'MS Sans Serif', Geneva, sans-serif"                   => "'MS Sans Serif', Geneva, sans-serif",
+        "'MS Serif', 'New York', sans-serif"                    => "'MS Serif', 'New York', sans-serif",
+        "'Palatino Linotype', 'Book Antiqua', Palatino, serif"  => "'Palatino Linotype', 'Book Antiqua', Palatino, serif",
+        "Tahoma,Geneva, sans-serif"                             => "Tahoma, Geneva, sans-serif",
+        "'Times New Roman', Times,serif"                        => "'Times New Roman', Times, serif",
+        "'Trebuchet MS', Helvetica, sans-serif"                 => "'Trebuchet MS', Helvetica, sans-serif",
+        "Verdana, Geneva, sans-serif"                           => "Verdana, Geneva, sans-serif",    
     );
-
+    
     /**
      * Field Constructor.
      *
@@ -495,6 +490,15 @@ class ReduxFramework_typography extends ReduxFramework {
         if (!empty($this->field['fonts'])){
             //Make the font keys in the array lowercase, for case-insensitive matching
             $lcFonts = array_change_key_case($this->field['fonts']);
+
+            // Rebuild font array with all keys stripped of spaces
+            $arr = array();
+            foreach ($lcFonts as $key => $value){
+                $key = str_replace(', ', ',', $key);
+                $arr[$key] = $value;
+            }
+            $lcFonts = $arr;
+            unset ($arr);
 
             // lowercase chosen font for matching purposes
             $lcFont = strtolower($font['font-family']);
