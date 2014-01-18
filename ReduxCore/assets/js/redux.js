22fad0356830c6fd78e993ffbdb3f62947aabb50
Dovy Paukstys <info@simplerain.com>
2014-01-18 16:14:58 +0100

    Fix for slides with numbers in IDs. Issue #569. Thanks @sjozsef.

--- ReduxCore/assets/js/redux.js
+++ ReduxCore/assets/js/redux.js
@@ -542,21 +542,22 @@ jQuery(document).ready(function($) {
 		}
 		window.onbeforeunload = null;
 	});	
-	jQuery('#expand_options').click(function(e) {
-		e.preventDefault();
-		var trigger = jQuery('#expand_options');
-		var width = jQuery('#redux-sidebar').width();
-		var id = jQuery('#redux-group-menu .active a').data('rel') + '_section_group';
+
+	function redux_expand_options(parent) {
+		console.log('here');
+		var trigger = parent.find('.expand_options');
+		var width = parent.find('.redux-sidebar').width();
+		var id = jQuery('.redux-group-menu .active a').data('rel') + '_section_group';
 		if (trigger.hasClass('expanded')) {
 			trigger.removeClass('expanded');
-			jQuery('.redux-main').removeClass('expand');
-			jQuery('#redux-sidebar').stop().animate({
+			parent.find('.redux-main').removeClass('expand');
+			parent.find('.redux-sidebar').stop().animate({
 				'margin-left': '0px'
 			}, 500);
-			jQuery('.redux-main').stop().animate({
+			parent.find('.redux-main').stop().animate({
 				'margin-left': width
 			}, 500);
-			jQuery('.redux-group-tab').each(function() {
+			parent.find('.redux-group-tab').each(function() {
 				if (jQuery(this).attr('id') !== id) {
 					jQuery(this).fadeOut('fast');
 				}
@@ -564,15 +565,22 @@ jQuery(document).ready(function($) {
 			// Show the only active one
 		} else {
 			trigger.addClass('expanded');
-			jQuery('.redux-main').addClass('expand');
-			jQuery('#redux-sidebar').stop().animate({
-				'margin-left': -width - 2
+			parent.find('.redux-main').addClass('expand');
+			parent.find('.redux-sidebar').stop().animate({
+				'margin-left': -width - 102
 			}, 500);
-			jQuery('.redux-main').stop().animate({
+			parent.find('.redux-main').stop().animate({
 				'margin-left': '0px'
 			}, 500);
-			jQuery('.redux-group-tab').fadeIn();
+			parent.find('.redux-group-tab').fadeIn();
 		}
+		return false;
+	}
+
+	jQuery('.expand_options').click(function(e) {
+		e.preventDefault();
+
+		redux_expand_options(jQuery(this).parents('.redux-container:first'));
 		return false;
 	});
 	jQuery('#redux-import').click(function(e) {
