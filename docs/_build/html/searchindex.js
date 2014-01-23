268282362c778e74ed4611e6a5f4881b553d0a06
Dovy Paukstys <info@simplerain.com>
2014-01-23 06:53:54 +0100

    Remove docs directory

--- docs/_build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to Redux Framework&#8217;s documentation!"],objects:{},titleterms:{redux:0,welcom:0,indic:0,framework:0,tabl:0,document:0}})
