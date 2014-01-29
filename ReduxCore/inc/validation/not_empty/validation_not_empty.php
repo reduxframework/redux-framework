cc92bfbf76ce6d292f6a4b1596e5e8545589148a
Dovy Paukstys <info@simplerain.com>
2014-01-29 15:35:46 -0700

    Added not_empty validation for issue #625.

---
+++ ReduxCore/inc/validation/not_empty/validation_not_empty.php
@@ -0,0 +1,40 @@
+<?php
+class Redux_Validation_not_empty extends ReduxFramework {	
+	
+	/**
+	 * Field Constructor.
+	 *
+	 * Required - must call the parent constructor, then assign field and value to vars, and obviously call the render field function
+	 *
+	 * @since ReduxFramework 1.0.0
+	*/
+	function __construct($field, $value, $current) {
+		
+		//parent::__construct();
+		$this->field = $field;
+		$this->field['msg'] = (isset($this->field['msg']))?$this->field['msg']:__('This field cannot be empty. Please provide a value.', 'redux-framework');
+		$this->value = $value;
+		$this->current = $current;
+		$this->validate();
+		
+	}//function
+	
+	
+	
+	/**
+	 * Field Render Function.
+	 *
+	 * Takes the vars and outputs the HTML for the field in the settings
+	 *
+	 * @since ReduxFramework 1.0.0
+	*/
+	function validate() {
+		
+		if( !isset( $this->value ) || empty( $this->value ) ){
+			$this->value = ( isset( $this->current ) ) ? $this->current : '';
+			$this->error = $this->field;
+		}
+		
+	}//function
+	
+}//class
