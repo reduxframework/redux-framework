f7fa08d785040dab541b33dd2c413ecd5d049d78
Kevin Provance <kevin.provance@gmail.com>
2014-02-06 13:36:10 -0500

    Fixed plgins using Redux from load failure.

--- redux-framework.php
+++ redux-framework.php
@@ -41,4 +41,11 @@ register_activation_hook( __FILE__, array( 'ReduxFrameworkPlugin', 'activate' ) 
 register_deactivation_hook( __FILE__, array( 'ReduxFrameworkPlugin', 'deactivate' ) );
 
 // Get plugin instance
-add_action( 'plugins_loaded', array( 'ReduxFrameworkPlugin', 'instance' ) );
+//add_action( 'plugins_loaded', array( 'ReduxFrameworkPlugin', 'instance' ) );
+
+// The above line prevents ReduxFramework from instancing until all plugins have loaded.
+// While this does not matter for themes, any plugin using Redux will not load properly.
+// Waiting until all plugsin have been loaded prevents the ReduxFramework class from 
+// being created, and fails the !class_exists('ReduxFramework') check in the sample_config.php, 
+// and thus prevents any plugin using Redux from loading their config file.
+ReduxFrameworkPlugin::instance();
