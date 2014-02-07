708a99755c79f3b1ea846a7aed63d73bc254d5d1
Kevin Provance <kevin.provance@gmail.com>
2014-02-06 23:02:17 -0500

    Fix for #689
    Specified default color not set when clicking the color box default button.

--- ReduxCore/inc/fields/border/field_border.php
+++ ReduxCore/inc/fields/border/field_border.php
@@ -58,8 +58,8 @@ class ReduxFramework_border extends ReduxFramework {
 			'top'				=> true,
 			'bottom'			=> true,
 			'all'				=> true,
-            'style'             => true,
-            'color'             => true,
+            		'style'             		=> true,
+            		'color'             		=> true,
 			'left'				=> true,
 			'right'				=> true,
 			);
@@ -172,9 +172,8 @@ class ReduxFramework_border extends ReduxFramework {
             **/
 
             if ( $this->field['color'] != false ):
-            	$default = isset( $this->field['border-color'] ) ? $this->field['border-color'] : "";
-            	$default = ( empty( $default ) && isset( $this->field['color'] ) ) ? $this->field['color'] : "";
-                echo '<input name="' . $this->parent->args['opt_name'] . '[' . $this->field['id'] . '][border-color]" id="' . $this->field['id'] . '-border" class="redux-border-color redux-color redux-color-init ' . $this->field['class'] . '"  type="text" value="' . $value['color'] . '"  data-default-color="' . $default . '" data-id="'.$this->field['id'].'" />';
+            	$default = isset($this->value['border-color']) ? $this->value['border-color'] : "";
+            	$default = ( empty($default) && isset($this->value['color']) ) ? $this->value['color'] : "#ffffff";                echo '<input name="' . $this->parent->args['opt_name'] . '[' . $this->field['id'] . '][border-color]" id="' . $this->field['id'] . '-border" class="redux-border-color redux-color redux-color-init ' . $this->field['class'] . '"  type="text" value="' . $value['color'] . '"  data-default-color="' . $default . '" data-id="'.$this->field['id'].'" />';
             endif;
 
 
