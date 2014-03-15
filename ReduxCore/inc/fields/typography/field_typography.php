2cad1df9ea854c31307a55d24ae61e36c8746604
KProvance <kevin.provance@gmail.com>
2014-03-15 09:34:18 -0400

    fix button_set multi.

--- ReduxCore/inc/fields/typography/field_typography.php
+++ ReduxCore/inc/fields/typography/field_typography.php
@@ -414,13 +414,13 @@ class ReduxFramework_typography {
          *
          */
         if (!isset($this->field['preview']) || $this->field['preview'] !== false):
-            if (isset($value['preview']['text'])) {
-                $g_text = $value['preview']['text'];
+            if (isset($this->field['preview']['text'])) {
+                $g_text = $this->field['preview']['text'];
             } else {
                 $g_text = '1 2 3 4 5 6 7 8 9 0  A B C D E F G H I J K L M N O P Q R S T U V W X Y Z  a b c d e f g h i j k l m n o p q r s t u v w x y z';
             }
-            if (isset($value['preview']['font-size'])) {
-                $g_size = 'style="font-size: ' . $value['preview']['font-size'] . ';"';
+            if (isset($this->field['preview']['font-size'])) {
+                $g_size = 'style="font-size: ' . $this->field['preview']['font-size'] . ';"';
             } else {
                 $g_size = '';
             }
