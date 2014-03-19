df031d851008e34bd806fdb584fd50ab27784856
Dovy <dovy@reduxframework.com>
2014-03-19 00:22:53 -0600

    Picky yml file

--- bin/update-mirrors-docs.sh
+++ bin/update-mirrors-docs.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_JOB_NUMBER" == *.1 ]]; then
 
 	# Update the mirror repo for composer/packagist
-	git push --mirror https://$(GH_TOKEN)@github.com/redux-framework/redux-framework.git  
+	git push --mirror https://${GH_TOKEN}@github.com/redux-framework/redux-framework.git  
 
 	# Re-Deploy the heroku demo app and pull the newest code
 	git clone git@heroku.com:redux-premium.git
