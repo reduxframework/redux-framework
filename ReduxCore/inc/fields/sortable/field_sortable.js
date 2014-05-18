545160455819dcbc01f463407111934646b59697
KProvance <kevin.provance@gmail.com>
2014-05-18 16:12:27 -0400

    more css adjustments, sortable fix Signed-off-by:KProvance <kevin.provance@gmail.com>

--- ReduxCore/inc/fields/sortable/field_sortable.js
+++ ReduxCore/inc/fields/sortable/field_sortable.js
@@ -4,10 +4,39 @@
     "use strict";
     
     $(document).ready(function() {
+        
+        var scroll = '';
+        var $scrollable = $(".redux-sorter");
+        function scrolling(){
+            if (scroll == 'up') {
+                $scrollable.scrollTop($scrollable.scrollTop()-20);
+                setTimeout(scrolling,50);
+            } else if (scroll == 'down'){
+                $scrollable.scrollTop($scrollable.scrollTop()+20);
+                setTimeout(scrolling,50);
+            }
+        }          
+        
         $(".redux-sortable").sortable({
             handle: ".drag",
             placeholder:    "placeholder",
             opacity: 0.7,
+            scroll: false,
+            out: function( event, ui ) {
+                if (!ui.helper) return;
+                if (ui.offset.top>0) {
+                    scroll='down';
+                } else {
+                    scroll='up';
+                }
+                scrolling();
+            },
+            over: function( event, ui ) {
+                scroll='';
+            },
+            deactivate:function( event, ui ) {
+                scroll='';
+            },                            
             update: function() {
                 redux_change($(this));
             }
