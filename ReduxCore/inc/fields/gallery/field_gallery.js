a1b878374fbe53e9afc89bc842bd7081b4f65120
Dovy <info@simplerain.com>
2014-06-10 15:31:45 -0600

    Gallery now loops.

--- ReduxCore/inc/fields/gallery/field_gallery.js
+++ ReduxCore/inc/fields/gallery/field_gallery.js
@@ -15,73 +15,93 @@
     );
 
     redux.field_objects.gallery.init = function( selector ) {
-//TODO - Make this loop!
-        // When the user clicks on the Add/Edit gallery button, we need to display the gallery editing
-        $( 'body' ).on(
-            {
-                click: function( event ) {
-                    var current_gallery = $( this ).closest( 'fieldset' );
 
-                    if ( event.currentTarget.id === 'clear-gallery' ) {
-                        //remove value from input
 
-                        var rmVal = current_gallery.find( '.gallery_values' ).val( '' );
+        if ( !selector ) {
+            selector = $( document ).find( '.redux-container-gallery' );
+        }
 
-                        //remove preview images
-                        current_gallery.find( ".screenshot" ).html( "" );
+        $( selector ).each(
+            function() {
+                var el = $( this );
+                var parent = el;
+                if ( !el.hasClass( 'redux-field-container' ) ) {
+                    parent = el.parents( '.redux-field-container:first' );
+                }
+                if ( parent.hasClass( 'redux-field-init' ) ) {
+                    parent.removeClass( 'redux-field-init' )
+                } else {
+                    return;
+                }
+                // When the user clicks on the Add/Edit gallery button, we need to display the gallery editing
+                el.on(
+                    {
+                        click: function( event ) {
+                            var current_gallery = $( this ).closest( 'fieldset' );
 
-                        return;
+                            if ( event.currentTarget.id === 'clear-gallery' ) {
+                                //remove value from input
 
-                    }
+                                var rmVal = current_gallery.find( '.gallery_values' ).val( '' );
 
-                    // Make sure the media gallery API exists
-                    if ( typeof wp === 'undefined' || !wp.media || !wp.media.gallery ) {
-                        return;
-                    }
-                    event.preventDefault();
+                                //remove preview images
+                                current_gallery.find( ".screenshot" ).html( "" );
 
-                    // Activate the media editor
-                    var $$ = $( this );
+                                return;
 
-                    var val = current_gallery.find( '.gallery_values' ).val();
-                    var final;
+                            }
 
-                    if ( !val ) {
-                        final = '[gallery ids="0"]';
-                    } else {
-                        final = '[gallery ids="' + val + '"]';
-                    }
+                            // Make sure the media gallery API exists
+                            if ( typeof wp === 'undefined' || !wp.media || !wp.media.gallery ) {
+                                return;
+                            }
+                            event.preventDefault();
 
-                    var frame = wp.media.gallery.edit( final );
+                            // Activate the media editor
+                            var $$ = $( this );
 
-                    // When the gallery-edit state is updated, copy the attachment ids across
-                    frame.state( 'gallery-edit' ).on(
-                        'update', function( selection ) {
+                            var val = current_gallery.find( '.gallery_values' ).val();
+                            var final;
 
-                            //clear screenshot div so we can append new selected images
-                            current_gallery.find( ".screenshot" ).html( "" );
+                            if ( !val ) {
+                                final = '[gallery ids="0"]';
+                            } else {
+                                final = '[gallery ids="' + val + '"]';
+                            }
 
-                            var element, preview_html = "", preview_img;
-                            var ids = selection.models.map(
-                                function( e ) {
-                                    element = e.toJSON();
-                                    preview_img = typeof element.sizes.thumbnail !== 'undefined' ? element.sizes.thumbnail.url : element.url;
-                                    preview_html = "<a class='of-uploaded-image' href='" + preview_img + "'><img class='redux-option-image' src='" + preview_img + "' alt='' /></a>";
-                                    current_gallery.find( ".screenshot" ).append( preview_html );
+                            var frame = wp.media.gallery.edit( final );
 
-                                    return e.id;
+                            // When the gallery-edit state is updated, copy the attachment ids across
+                            frame.state( 'gallery-edit' ).on(
+                                'update', function( selection ) {
+
+                                    //clear screenshot div so we can append new selected images
+                                    current_gallery.find( ".screenshot" ).html( "" );
+
+                                    var element, preview_html = "", preview_img;
+                                    var ids = selection.models.map(
+                                        function( e ) {
+                                            element = e.toJSON();
+                                            preview_img = typeof element.sizes.thumbnail !== 'undefined' ? element.sizes.thumbnail.url : element.url;
+                                            preview_html = "<a class='of-uploaded-image' href='" + preview_img + "'><img class='redux-option-image' src='" + preview_img + "' alt='' /></a>";
+                                            current_gallery.find( ".screenshot" ).append( preview_html );
+
+                                            return e.id;
+                                        }
+                                    );
+
+                                    current_gallery.find( '.gallery_values' ).val( ids.join( ',' ) );
+                                    redux_change( current_gallery.find( '.gallery_values' ) );
+
                                 }
                             );
 
-                            current_gallery.find( '.gallery_values' ).val( ids.join( ',' ) );
-                            redux_change( current_gallery.find( '.gallery_values' ) );
-
+                            return false;
                         }
-                    );
-
-                    return false;
-                }
-            }, '.gallery-attachments'
+                    }, '.gallery-attachments'
+                );
+            }
         );
+
     };
 })( jQuery );
