9334f4b33d62e0473ac08b592baeb20acfd7bb8d
KProvance <kevin.provance@gmail.com>
2014-06-12 12:17:55 -0400

    3.3.1.2 Signed-off-by:KProvance <kevin.provance@gmail.com>

--- ReduxCore/framework.php
+++ ReduxCore/framework.php
@@ -64,7 +64,7 @@
             // ATTENTION DEVS
             // Please update the build number with each push, no matter how small.
             // This will make for easier support when we ask users what version they are using.
-            public static $_version = '3.3.1.1';
+            public static $_version = '3.3.1.2';
             public static $_dir;
             public static $_url;
             public static $_upload_dir;
