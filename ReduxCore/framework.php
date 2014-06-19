301aa3c47b5e970925039f86370bbb23b29dae71
Gregory Karpinsky <gregory@tiv.net>
2014-06-19 14:43:20 -0400

    Version up, as requested

--- ReduxCore/framework.php
+++ ReduxCore/framework.php
@@ -63,7 +63,7 @@
             // ATTENTION DEVS
             // Please update the build number with each push, no matter how small.
             // This will make for easier support when we ask users what version they are using.
-            public static $_version = '3.3.2.1';
+            public static $_version = '3.3.2.2';
             public static $_dir;
             public static $_url;
             public static $_upload_dir;
