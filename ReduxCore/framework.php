7e1cd177da1a22227087bf4b661211a7bc253cab
Dovy <dovy@reduxframework.com>
2014-07-29 01:50:44 -0600

    Ooops, update the script while we're at it.

--- ReduxCore/framework.php
+++ ReduxCore/framework.php
@@ -1407,7 +1407,7 @@
 
                             (function() {
                                 var wf = document.createElement( 'script' );
-                                wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.5.0/webfont.js';
+                                wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.5.3/webfont.js';
                                 wf.type = 'text/javascript';
                                 wf.async = 'true';
                                 var s = document.getElementsByTagName( 'script' )[0];
