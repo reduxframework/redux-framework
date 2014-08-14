f48fd078f87e76e95ae50b8bde9a8c41bea8872a
KProvance <kevin.provance@gmail.com>
2014-08-14 15:00:38 -0400

    3.3.6.1 Signed-off-by:KProvance <kevin.provance@gmail.com>

--- ReduxCore/framework.php
+++ ReduxCore/framework.php
@@ -484,15 +484,10 @@
 
             // Fix conflicts with Visual Composer.
             public function vc_fixes() {
-
                 if (redux_helpers::isFieldInUse ( $this, 'ace_editor' )) {
                     wp_dequeue_script( 'wpb_ace' );
                     wp_deregister_script( 'wpb_ace' );                
                 }
-
-                wp_dequeue_script( 'wpb_ace' );
-                wp_deregister_script( 'wpb_ace' );
-
             }
 
             public function network_admin_bar( $wp_admin_bar ) {
