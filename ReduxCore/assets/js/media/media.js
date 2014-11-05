9962e26a96ef68034568a24d8a7a160f0d8df8dd
KProvance <kevin.provance@gmail.com>
2014-11-05 04:07:44 -0500

    cleanup Signed-off-by:KProvance <kevin.provance@gmail.com>

--- ReduxCore/assets/js/media/media.js
+++ ReduxCore/assets/js/media/media.js
@@ -10,14 +10,14 @@
 (function($){
     "use strict";
 
-    redux.field_objects = redux.field_objects || {};
-    redux.field_objects.media = redux.field_objects.media || {};
+    redux.field_objects         = redux.field_objects || {};
+    redux.field_objects.media   = redux.field_objects.media || {};
 
     var isFiltered;
 
     $( document ).ready(
         function() {
-            //redux.field_objects.media.init();
+
         }
     );
 
@@ -29,19 +29,19 @@
             function() {
                 var el = $( this );
                 var parent = el;
-                
+
                 if ( !el.hasClass( 'redux-field-container' ) ) {
                     parent = el.parents( '.redux-field-container:first' );
                 }
-                
+
                 if ( parent.hasClass( 'redux-field-init' ) ) {
                     parent.removeClass( 'redux-field-init' );
                 } else {
                     return;
                 }
-                
+
                 isFiltered = false;
-                
+
                 // Remove the image button
                 el.find( '.remove-image, .remove-file' ).unbind( 'click' ).on(
                     'click', function() {
@@ -74,7 +74,7 @@
 
         // Get library filter data
         var filter = $( selector ).find('.library-filter').data('lib-filter');
-        
+
         // Must exist to do decoding
         if (filter !== undefined) {
             if (filter !== ''){
@@ -132,9 +132,9 @@
                 if (isFiltered === true) {
                     data.mode = 0;
                 }
-                
+
                 if (data.mode === 0) {
-                    
+
                 } else {
                     if ( data.mode !== false) {
                         if (attachment.attributes.type !== data.mode) {
