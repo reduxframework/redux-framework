215af24ce96b4166c07f624f49c1bc69c29bad76
KProvance <kevin.provance@gmail.com>
2014-11-18 14:09:41 -0500

    #1848 Signed-off-by:KProvance <kevin.provance@gmail.com>

---
+++ ReduxCore/redux-extensions/loader.php
@@ -0,0 +1,29 @@
+<?php
+
+// Replace {$redux_opt_name} with your opt_name.
+// Also be sure to change this function name!
+
+if(!function_exists('redux_register_custom_extension_loader')) :
+    function redux_register_custom_extension_loader($ReduxFramework) {
+        $path = dirname( __FILE__ ) . '/extensions/';
+        $folders = scandir( $path, 1 );        
+        foreach($folders as $folder) {
+            if ($folder === '.' or $folder === '..' or !is_dir($path . $folder) ) {
+                continue;   
+            } 
+            $extension_class = 'ReduxFramework_Extension_' . $folder;
+            if( !class_exists( $extension_class ) ) {
+                // In case you wanted override your override, hah.
+                $class_file = $path . $folder . '/extension_' . $folder . '.php';
+                $class_file = apply_filters( 'redux/extension/'.$ReduxFramework->args['opt_name'].'/'.$folder, $class_file );
+                if ( file_exists( $class_file ) ) {
+                    require_once( $class_file );
+                    $extension = new $extension_class( $ReduxFramework );
+                }
+            }
+        }
+    }
+    // Modify {$redux_opt_name} to match your opt_name
+    add_action("redux/extensions/{$redux_opt_name}/before", 'redux_register_custom_extension_loader', 0);
+endif;
+
