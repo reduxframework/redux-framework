a350a62e059ef126478b15059b383e324b463e1e
KProvance <kevin.provance@gmail.com>
2015-01-12 01:53:52 -0500

    #1980 Signed-off-by:KProvance <kevin.provance@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Redux Framework Changelog
 
+## 3.3.10.3
+* Fixed:    #1980 - JS errors with sorter & sortable.
+
 ## 3.3.10.2
 * Fixed:    #1979 - Goole fonts not properly loading in typography dropdown.
 
--- class.redux-plugin.php
+++ class.redux-plugin.php
@@ -25,7 +25,7 @@
              * @const       string VERSION The plugin version, used for cache-busting and script file references
              * @since       3.0.0
              */
-            const VERSION = '3.3.10.2';
+            const VERSION = '3.3.10.3';
 
             /**
              * @access      protected
