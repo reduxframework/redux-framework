dcf323ad977e5e069c919042aa949578c6c6c870
Dovy <dovy@reduxframework.com>
2015-02-05 13:08:14 -0700

    Fix as per required by WP.org. #2031

--- ReduxCore/inc/validation/css/validation_css.php
+++ ReduxCore/inc/validation/css/validation_css.php
@@ -11,7 +11,7 @@
              */
             function __construct( $parent, $field, $value, $current ) {
 
-                $this ->parent = $parent;
+                $this->parent  = $parent;
                 $this->field   = $field;
                 $this->value   = $value;
                 $this->current = $current;
@@ -29,13 +29,47 @@
              */
             function validate() {
 
-                // Strip all html
-                $data = $this->value;
-                
-                $data = wp_filter_nohtml_kses( $data );
-                $data = str_replace( '&gt;', '>', $data );
+                require_once( dirname( __FILE__ ) . '/csstidy/class.csstidy.php' );
 
-                $this->value = $data;
+                $csstidy = new csstidy();
+
+                $csstidy->set_cfg( 'remove_bslash', false );
+                $csstidy->set_cfg( 'compress_colors', false );
+                $csstidy->set_cfg( 'compress_font-weight', false );
+                $csstidy->set_cfg( 'optimise_shorthands', 0 );
+                $csstidy->set_cfg( 'remove_last_;', false );
+                $csstidy->set_cfg( 'case_properties', false );
+                $csstidy->set_cfg( 'discard_invalid_properties', true );
+                $csstidy->set_cfg( 'css_level', 'CSS3.0' );
+                $csstidy->set_cfg( 'preserve_css', true );
+                $csstidy->set_cfg( 'template', dirname( __FILE__ ) . '/csstidy/wordpress-standard.tpl' );
+
+                $css = $orig = $this->value;
+
+                $css = preg_replace( '/\\\\([0-9a-fA-F]{4})/', '\\\\\\\\$1', $prev = $css );
+
+                if ( $css != $prev ) {
+                    $this->warning = true;
+                }
+
+                // Some people put weird stuff in their CSS, KSES tends to be greedy
+                $css = str_replace( '<=', '&lt;=', $css );
+                // Why KSES instead of strip_tags?  Who knows?
+                $css = wp_kses_split( $prev = $css, array(), array() );
+                $css = str_replace( '&gt;', '>', $css ); // kses replaces lone '>' with &gt;
+                // Why both KSES and strip_tags?  Because we just added some '>'.
+                $css = strip_tags( $css );
+
+                if ( $css != $prev ) {
+                    $this->warning = true;
+                }
+
+                $csstidy->parse( $css );
+                $this->value = $csstidy->print->plain();
+
+                if ( isset( $this->warning ) && $this->warning ) {
+                    $this->warning = __( 'Unsafe strings were found in your CSS and have been filtered out.', 'redux-framework' );
+                }
 
             } //function
         } //class
