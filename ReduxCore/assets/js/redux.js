c159db60169ef584093d51bab6444fccefccdcdc
Dovy <dovy@reduxframework.com>
2015-02-26 12:50:20 -0700

    Cleaner fix.

--- ReduxCore/assets/js/redux.js
+++ ReduxCore/assets/js/redux.js
@@ -59,8 +59,9 @@
         if ( redux.fields.hasOwnProperty( "editor" ) ) {
             $.each(
                 redux.fields.editor, function( $key, $index ) {
-                    if ( !$( document.getElementById( redux.args.opt_name + '-' + $key ) ).hasClass( 'redux-field-init' ) ) {
-                        tinyMCE.get( $key ).save();
+                    var editor = tinyMCE.get( $key );
+                    if ( editor ) {
+                        editor.save();
                     }
                 }
             );
