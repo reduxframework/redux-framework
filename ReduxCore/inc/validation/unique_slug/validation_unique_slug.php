c6c70f04d85eb9386c62499bf5b74a4461178159
Dovy <dovy@reduxframework.com>
2015-02-26 12:05:51 -0700

    OOps. Useless code.

--- ReduxCore/inc/validation/unique_slug/validation_unique_slug.php
+++ ReduxCore/inc/validation/unique_slug/validation_unique_slug.php
@@ -23,47 +23,43 @@
 
             function validate() {
 
-                if ( ! empty( $this->value ) && $this->value != $this->current ) {
-                    // Code taken from post.php in WordPress core
+                global $wpdb, $wp_rewrite;
 
-                    global $wpdb, $wp_rewrite;
+                $slug = $this->value;
 
-                    $slug = $this->value;
-
-                    $feeds = $wp_rewrite->feeds;
-                    if ( ! is_array( $feeds ) ) {
-                        $feeds = array();
-                    }
-
-                    // Post slugs must be unique across all posts.
-                    $check_sql       = "SELECT post_name FROM $wpdb->posts WHERE post_name = %s LIMIT 1";
-                    $post_name_check = $wpdb->get_var( $wpdb->prepare( $check_sql, $slug ) );
-
-                    /**
-                     * Filter whether the post slug would be bad as a flat slug.
-                     *
-                     * @since 3.1.0
-                     *
-                     * @param bool   $bad_slug  Whether the post slug would be bad as a flat slug.
-                     * @param string $slug      The post slug.
-                     * @param string $post_type Post type.
-                     */
-                    if ( $post_name_check || in_array( $slug, $feeds ) || apply_filters( 'wp_unique_post_slug_is_bad_attachment_slug', false, $slug ) ) {
-                        $suffix = 2;
-                        do {
-                            $alt_post_name   = _truncate_post_slug( $slug, 200 - ( strlen( $suffix ) + 1 ) ) . "-$suffix";
-                            $post_name_check = $wpdb->get_var( $wpdb->prepare( $check_sql, $alt_post_name ) );
-                            $suffix ++;
-                        } while ( $post_name_check );
-                        $slug               = $alt_post_name;
-                        $this->value        = ( isset( $this->current ) ) ? $this->current : '';
-                        $this->field['msg'] = sprintf( $this->field['msg'], $slug );
-                        $this->error        = $this->field;
-                    } else if ( isset( $this->field['flush_permalinks'] ) && $this->field['flush_permalinks'] == true ) {
-                        add_action( 'init', array( $this, 'flush_permalinks' ), 99 );
-                    }
+                $feeds = $wp_rewrite->feeds;
+                if ( ! is_array( $feeds ) ) {
+                    $feeds = array();
                 }
 
+                // Post slugs must be unique across all posts.
+                $check_sql       = "SELECT post_name FROM $wpdb->posts WHERE post_name = %s LIMIT 1";
+                $post_name_check = $wpdb->get_var( $wpdb->prepare( $check_sql, $slug ) );
+
+                /**
+                 * Filter whether the post slug would be bad as a flat slug.
+                 *
+                 * @since 3.1.0
+                 *
+                 * @param bool   $bad_slug  Whether the post slug would be bad as a flat slug.
+                 * @param string $slug      The post slug.
+                 * @param string $post_type Post type.
+                 */
+                if ( $post_name_check || in_array( $slug, $feeds ) || apply_filters( 'wp_unique_post_slug_is_bad_attachment_slug', false, $slug ) ) {
+                    $suffix = 2;
+                    do {
+                        $alt_post_name   = _truncate_post_slug( $slug, 200 - ( strlen( $suffix ) + 1 ) ) . "-$suffix";
+                        $post_name_check = $wpdb->get_var( $wpdb->prepare( $check_sql, $alt_post_name ) );
+                        $suffix ++;
+                    } while ( $post_name_check );
+                    $slug               = $alt_post_name;
+                    $this->value        = ( isset( $this->current ) ) ? $this->current : '';
+                    $this->field['msg'] = sprintf( $this->field['msg'], $slug );
+                    $this->error        = $this->field;
+                } else if ( isset( $this->field['flush_permalinks'] ) && $this->field['flush_permalinks'] == true ) {
+                    add_action( 'init', array( $this, 'flush_permalinks' ), 99 );
+                }
+                
             } //function
 
             function flush_permalinks() {
