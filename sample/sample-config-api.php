b37ceed1c584e8dd31b30069c43ce08ae0865f1a
KProvance <kevin.provance@gmail.com>
2015-03-31 15:55:10 -0400

    update sample-config-api Signed-off-by:KProvance <kevin.provance@gmail.com>

--- sample/sample-config-api.php
+++ sample/sample-config-api.php
@@ -33,9 +33,9 @@
     $sample_patterns_url  = ReduxFramework::$_url . '../sample/patterns/';
     $sample_patterns      = array();
 
-    if ( is_dir( $sample_patterns_path ) ) :
+    if ( is_dir( $sample_patterns_path ) ) {
 
-        if ( $sample_patterns_dir = opendir( $sample_patterns_path ) ) :
+        if ( $sample_patterns_dir = opendir( $sample_patterns_path ) ) {
             $sample_patterns = array();
 
             while ( ( $sample_patterns_file = readdir( $sample_patterns_dir ) ) !== false ) {
@@ -49,9 +49,31 @@
                     );
                 }
             }
-        endif;
-    endif;
+        }
+    }
 
+    /*
+     *
+     * --> Action hook examples
+     *
+     */
+    
+    // If Redux is running as a plugin, this will remove the demo notice and links
+    //add_action( 'redux/loaded', 'remove_demo' );
+
+    // Function to test the compiler hook and demo CSS output.
+    // Above 10 is a priority, but 2 in necessary to include the dynamically generated CSS to be sent to the function.
+    //add_filter('redux/options/' . $opt_name . '/compiler', 'compiler_action', 10, 3);
+
+    // Change the arguments after they've been declared, but before the panel is created
+    //add_filter('redux/options/' . $opt_name . '/args', 'change_arguments' );
+
+    // Change the default value of a field after it's been set, but before it's been useds
+    //add_filter('redux/options/' . $opt_name . '/defaults', 'change_defaults' );
+
+    // Dynamically add a section. Can be also used to modify sections/fields
+    //add_filter('redux/options/' . $opt_name . '/sections', 'dynamic_section');
+    
 
     /**
      * ---> SET ARGUMENTS
@@ -2063,152 +2085,23 @@
 
     exit();
 
-
-    if ( ! class_exists( 'Redux_Framework_sample_config' ) ) {
-
-        class Redux_Framework_sample_config {
-            public $opt_name;
-
-            public function __construct() {
-
-
-                // If Redux is running as a plugin, this will remove the demo notice and links
-                //add_action( 'redux/loaded', array( $this, 'remove_demo' ) );
-
-                // Function to test the compiler hook and demo CSS output.
-                // Above 10 is a priority, but 2 in necessary to include the dynamically generated CSS to be sent to the function.
-                //add_filter('redux/options/'.$this->args['opt_name'].'/compiler', array( $this, 'compiler_action' ), 10, 3);
-
-                // Change the arguments after they've been declared, but before the panel is created
-                //add_filter('redux/options/'.$this->args['opt_name'].'/args', array( $this, 'change_arguments' ) );
-
-                // Change the default value of a field after it's been set, but before it's been useds
-                //add_filter('redux/options/'.$this->args['opt_name'].'/defaults', array( $this,'change_defaults' ) );
-
-                // Dynamically add a section. Can be also used to modify sections/fields
-                //add_filter('redux/options/' . $this->args['opt_name'] . '/sections', array($this, 'dynamic_section'));
-
-            }
-
-            /**
-             * This is a test function that will let you see when the compiler hook occurs.
-             * It only runs if a field    set with compiler=>true is changed.
-             * */
-            function compiler_action( $options, $css, $changed_values ) {
-                echo '<h1>The compiler hook has run!</h1>';
-                echo "<pre>";
-                print_r( $changed_values ); // Values that have changed since the last save
-                echo "</pre>";
-                //print_r($options); //Option values
-                //print_r($css); // Compiler selector CSS values  compiler => array( CSS SELECTORS )
-            }
-
-            /**
-             * Custom function for filtering the sections array. Good for child themes to override or add to the sections.
-             * Simply include this function in the child themes functions.php file.
-             * NOTE: the defined constants for URLs, and directories will NOT be available at this point in a child theme,
-             * so you must use get_template_directory_uri() if you want to use any of the built in icons
-             * */
-            function dynamic_section( $sections ) {
-                //$sections = array();
-                $sections[] = array(
-                    'title'  => __( 'Section via hook', 'redux-framework-demo' ),
-                    'desc'   => __( '<p class="description">This is a section created by adding a filter to the sections array. Can be used by child themes to add/remove sections from the options.</p>', 'redux-framework-demo' ),
-                    'icon'   => 'el el-paper-clip',
-                    // Leave this as a blank section, no options just some intro text set above.
-                    'fields' => array()
-                );
-
-                return $sections;
-            }
-
-            /**
-             * Filter hook for filtering the args. Good for child themes to override or add to the args array. Can also be used in other functions.
-             * */
-            function change_arguments( $args ) {
-                //$args['dev_mode'] = true;
-
-                return $args;
-            }
-
-            /**
-             * Filter hook for filtering the default value of any given field. Very useful in development mode.
-             * */
-            function change_defaults( $defaults ) {
-                $defaults['str_replace'] = 'Testing filter hook!';
-
-                return $defaults;
-            }
-
-            // Remove the demo link and the notice of integrated demo from the redux-framework plugin
-            function remove_demo() {
-
-                // Used to hide the demo mode link from the plugin page. Only used when Redux is a plugin.
-                if ( class_exists( 'ReduxFrameworkPlugin' ) ) {
-                    remove_filter( 'plugin_row_meta', array(
-                        ReduxFrameworkPlugin::instance(),
-                        'plugin_metalinks'
-                    ), null, 2 );
-
-                    // Used to hide the activation notice informing users of the demo panel. Only used when Redux is a plugin.
-                    remove_action( 'admin_notices', array( ReduxFrameworkPlugin::instance(), 'admin_notices' ) );
-                }
-            }
-
-            public function validate_callback_function( $field, $value, $existing_value ) {
-                $error = true;
-                $value = 'just testing';
-
-                /*
-              do your validation
-
-              if(something) {
-                $value = $value;
-              } elseif(something else) {
-                $error = true;
-                $value = $existing_value;
-                
-              }
-             */
-
-                $return['value'] = $value;
-                $field['msg']    = 'your custom error message';
-                if ( $error == true ) {
-                    $return['error'] = $field;
-                }
-
-                return $return;
-            }
-
-            public static function class_field_callback( $field, $value ) {
-                print_r( $field );
-                echo '<br/>CLASS CALLBACK';
-                print_r( $value );
-            }
-
-        }
-
-        global $reduxConfig;
-        $reduxConfig = new Redux_Framework_sample_config();
-    } else {
-        echo "The class named Redux_Framework_sample_config has already been called. <strong>Developers, you need to prefix this class with your company name or you'll run into problems!</strong>";
-    }
-
     /**
-     * Custom function for the callback referenced above
-     */
-    if ( ! function_exists( 'redux_my_custom_field' ) ):
-        function redux_my_custom_field( $field, $value ) {
-            print_r( $field );
-            echo '<br/>';
-            print_r( $value );
-        }
-    endif;
-
+     * This is a test function that will let you see when the compiler hook occurs.
+     * It only runs if a field    set with compiler=>true is changed.
+     * */
+    function compiler_action( $options, $css, $changed_values ) {
+        echo '<h1>The compiler hook has run!</h1>';
+        echo "<pre>";
+        print_r( $changed_values ); // Values that have changed since the last save
+        echo "</pre>";
+        //print_r($options); //Option values
+        //print_r($css); // Compiler selector CSS values  compiler => array( CSS SELECTORS )
+    }    
+    
     /**
      * Custom function for the callback validation referenced above
      * */
-    if ( ! function_exists( 'redux_validate_callback_function' ) ):
+    if ( ! function_exists( 'redux_validate_callback_function' ) ){
         function redux_validate_callback_function( $field, $value, $existing_value ) {
             $error = true;
             $value = 'just testing';
@@ -2235,4 +2128,98 @@
 
             return $return;
         }
-    endif;
+    }
+    
+    /**
+     * Custom function for the callback referenced above
+     */
+    if ( ! function_exists( 'redux_my_custom_field' ) ){
+        function redux_my_custom_field( $field, $value ) {
+            print_r( $field );
+            echo '<br/>';
+            print_r( $value );
+        }
+    }
+
+    /**
+     * Custom function for filtering the sections array. Good for child themes to override or add to the sections.
+     * Simply include this function in the child themes functions.php file.
+     * NOTE: the defined constants for URLs, and directories will NOT be available at this point in a child theme,
+     * so you must use get_template_directory_uri() if you want to use any of the built in icons
+     * */
+    function dynamic_section( $sections ) {
+        //$sections = array();
+        $sections[] = array(
+            'title'  => __( 'Section via hook', 'redux-framework-demo' ),
+            'desc'   => __( '<p class="description">This is a section created by adding a filter to the sections array. Can be used by child themes to add/remove sections from the options.</p>', 'redux-framework-demo' ),
+            'icon'   => 'el el-paper-clip',
+            // Leave this as a blank section, no options just some intro text set above.
+            'fields' => array()
+        );
+
+        return $sections;
+    }    
+    
+    /**
+     * Filter hook for filtering the args. Good for child themes to override or add to the args array. Can also be used in other functions.
+     * */
+    function change_arguments( $args ) {
+        //$args['dev_mode'] = true;
+
+        return $args;
+    }
+
+    /**
+     * Filter hook for filtering the default value of any given field. Very useful in development mode.
+     * */
+    function change_defaults( $defaults ) {
+        $defaults['str_replace'] = 'Testing filter hook!';
+
+        return $defaults;
+    }
+
+    // Remove the demo link and the notice of integrated demo from the redux-framework plugin
+    function remove_demo() {
+
+        // Used to hide the demo mode link from the plugin page. Only used when Redux is a plugin.
+        if ( class_exists( 'ReduxFrameworkPlugin' ) ) {
+            remove_filter( 'plugin_row_meta', array(
+                ReduxFrameworkPlugin::instance(),
+                'plugin_metalinks'
+            ), null, 2 );
+
+            // Used to hide the activation notice informing users of the demo panel. Only used when Redux is a plugin.
+            remove_action( 'admin_notices', array( ReduxFrameworkPlugin::instance(), 'admin_notices' ) );
+        }
+    }
+    
+//    function validate_callback_function( $field, $value, $existing_value ) {
+//        $error = true;
+//        $value = 'just testing';
+//
+//        /*
+//      do your validation
+//
+//      if(something) {
+//        $value = $value;
+//      } elseif(something else) {
+//        $error = true;
+//        $value = $existing_value;
+//
+//      }
+//     */
+//
+//        $return['value'] = $value;
+//        $field['msg']    = 'your custom error message';
+//        if ( $error == true ) {
+//            $return['error'] = $field;
+//        }
+//
+//        return $return;
+//    }
+
+    function class_field_callback( $field, $value ) {
+        print_r( $field );
+        echo '<br/>CLASS CALLBACK';
+        print_r( $value );
+    }
