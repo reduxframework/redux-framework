78f41e6a0d3eedfe9e57da740afcbfe7c6148f7c
Dovy <dovy@reduxframework.com>
2015-04-01 12:15:14 -0600

    Support text.

--- ReduxCore/inc/welcome/views/support.php
+++ ReduxCore/inc/welcome/views/support.php
@@ -69,59 +69,136 @@
 
                 </div>
                 <div class="is_user">
+                    <p align="left">Listed below are the Wordpress panels installed on your site that utilize Redux
+                        Framework. You will need to direct your support query to the author of the listed products directly as Redux
+                        Framework does not directly support products created with our framework. It is the
+                        responsibility of the plugin or theme developer to support their work. Should the problem be
+                        with our framework, the developer needs to contact us directly. Thank you.</p>
                     <?php
 
-                        $redux = ReduxFrameworkInstances::get_all_instances();
+                        $redux        = ReduxFrameworkInstances::get_all_instances();
+                        if (!empty($redux)) {
+                            echo '<code>';
+                            foreach ( $redux as $panel ) {
+                                echo '<strong>'.$panel->args['display_name'].'</strong>';
+                                if (!empty($panel->args['display_version'])) {
+                                    echo ' v'.$panel->args['display_version'];
+                                }
 
-
-                        foreach($redux as $panel) {
-                            echo "Name: ".$panel->args['display_name'].' v.'.$panel->args['display_version'], PHP_EOL;
-                        }
-                    ?>
-
-                    <ul id="toDebug">
-                        <?php
-
-                            $active_plugins = (array) get_option( 'active_plugins', array() );
-
-                            if ( is_multisite() ) {
-                                $active_plugins = array_merge( $active_plugins, get_site_option( 'active_sitewide_plugins', array() ) );
                             }
+                            echo '</code>';
+                        }
 
 
-                            foreach ( $active_plugins as $plugin ) :
-                                $plugin_data           = @get_plugin_data( WP_PLUGIN_DIR . '/' . $plugin );
-                                $dirname               = dirname( $plugin );
-                                $plugin_data['folder'] = dirname( $plugin );
-
-                                ?>
-                                <li><input class="checkbox plugins" name="type[]" id="p<?php echo $dirname; ?>"
-                                           type="checkbox"
-                                           value="<?php echo urlencode( json_encode( $plugin_data ) ); ?>"><label
-                                        for="p<?php echo $dirname; ?>"><strong><?php _e( 'Plugin', 'redux-framework' );?>
-                                            : </strong> <?php echo $plugin_data['Name']; ?>
-                                        <small>v<?php echo $plugin_data['Version']; ?></small>
-                                    </label>
-                                </li>
-
-                            <?php
-                            endforeach;
-
-                            $active_theme = wp_get_theme()
-                        ?>
-                        <li><input class="checkbox theme" name="type[]"
-                                   id="<?php echo sanitize_html_class( $active_theme->Name ); ?>"
-                                   type="checkbox" data='<?php echo json_encode( $active_theme ); ?>'
-                                   value="<?php echo urlencode( json_encode( $active_theme ) ); ?>"><label
-                                for="<?php echo sanitize_html_class( $active_theme->Name ); ?>"><strong><?php _e( 'Theme', 'redux-framework' ); ?>
-                                    : </strong> <?php echo $active_theme->Name; ?>
-                                <small>v<?php echo $active_theme->Version; ?></small>
-                            </label>
-                        </li>
-                    </ul>
-                    <p>
-                        <a href="https://github.com/reduxframework/redux-framework/issues">https://github.com/reduxframework/redux-framework/issues</a><br/>
-                        Provide us with details about your issue as well as the following support hash code:
+                    //
+                    //    $t            = wp_get_theme();
+                    //    $active_theme = array(
+                    //        'name'       => $t->get( 'Name' ),
+                    //        'version'    => $t->get( 'Version' ),
+                    //        'template'   => $t->get( 'Template' ),
+                    //        'productURL' => $t->get( 'ThemeURI' ),
+                    //        'author'     => $t->get( 'Author' ),
+                    //        'authorURL'  => $t->get( 'AuthorURI' ),
+                    //    );
+                    //    $parent_theme = array();
+                    //
+                    //    if ( ! empty( $active_theme['Template'] ) ) {
+                    //        $pt           = wp_get_theme( $active_theme['Template'] );
+                    //        $parent_theme = array(
+                    //            'name'       => $pt->get( 'Name' ),
+                    //            'version'    => $pt->get( 'Version' ),
+                    //            'template'   => $pt->get( 'Template' ),
+                    //            'productURL' => $pt->get( 'ThemeURI' ),
+                    //            'author'     => $pt->get( 'Author' ),
+                    //            'authorURL'  => $pt->get( 'AuthorURI' ),
+                    //        );
+                    //    }
+                    //
+                    //
+                    //    $products = array( 'plugins' => array(), 'theme' => array(), 'unknown' => array() );
+                    //
+                    //    $active_plugins = (array) get_option( 'active_plugins', array() );
+                    //
+                    //    if ( is_multisite() ) {
+                    //        $active_plugins = array_merge( $active_plugins, get_site_option( 'active_sitewide_plugins', array() ) );
+                    //    }
+                    //
+                    //    $plugins = array();
+                    //
+                    //    foreach ( $active_plugins as $plugin ) :
+                    //        $plugin_data                     = @get_plugin_data( WP_PLUGIN_DIR . '/' . $plugin );
+                    //        $plugins[ $plugin_data['Name'] ] = array(
+                    //            'name'       => $plugin_data['Name'],
+                    //            'title'      => $plugin_data['Title'],
+                    //            'version'    => $plugin_data['Version'],
+                    //            'productURL' => $plugin_data['PluginURI'],
+                    //            'author'     => $plugin_data['Author'],
+                    //            'authorURL'  => $plugin_data['AuthorURI'],
+                    //        );
+                    //    endforeach;
+                    //
+                    //
+                    //    foreach ( $redux as $panel ) {
+                    //        if ( $active_theme == $panel->args['display_name'] ) {
+                    //            $products['theme'] = $active_theme;
+                    //        } else if ( $parent_theme == $panel->args['display_name'] ) {
+                    //            $products['theme'] = $parent_theme;
+                    //        } else if ( isset( $plugins[ $panel->args['display_name'] ] ) ) {
+                    //            $products['plugins'][] = array(
+                    //                'name'    => $panel->args['display_name'],
+                    //                'version' => $panel->args['display_version']
+                    //            );
+                    //        } else {
+                    //            $products['unknown'][] = array(
+                    //                'name'    => $panel->args['display_name'],
+                    //                'version' => $panel->args['display_version']
+                    //            );
+                    //        }
+                    //    }
+                    //    print_r( $products );
+                    //?>
+                    <!---->
+                    <!--<ul id="toDebug">-->
+                    <!--    --><?php
+                    //
+                    //        $active_plugins = (array) get_option( 'active_plugins', array() );
+                    //
+                    //        if ( is_multisite() ) {
+                    //            $active_plugins = array_merge( $active_plugins, get_site_option( 'active_sitewide_plugins', array() ) );
+                    //        }
+                    //
+                    //        foreach ( $active_plugins as $plugin ) :
+                    //            $plugin_data           = @get_plugin_data( WP_PLUGIN_DIR . '/' . $plugin );
+                    //            $dirname               = dirname( $plugin );
+                    //            $plugin_data['folder'] = dirname( $plugin );
+                    //
+                    //            ?>
+                    <!--            <li><input class="checkbox plugins" name="type[]" id="p--><?php //echo $dirname; ?><!--"-->
+                    <!--                       type="checkbox"-->
+                    <!--                       value="--><?php //echo urlencode( json_encode( $plugin_data ) ); ?><!--"><label-->
+                    <!--                    for="p--><?php //echo $dirname; ?><!--"><strong>--><?php //_e( 'Plugin', 'redux-framework' );?>
+                    <!--                        : </strong> --><?php //echo $plugin_data['Name']; ?>
+                    <!--                    <small>v--><?php //echo $plugin_data['Version']; ?><!--</small>-->
+                    <!--                </label>-->
+                    <!--            </li>-->
+                    <!---->
+                    <!--        --><?php
+                    //        endforeach;
+                    //
+                    //        $active_theme = wp_get_theme();
+                    //    ?>
+                    <!--    <li><input class="checkbox theme" name="type[]"-->
+                    <!--               id="--><?php //echo sanitize_html_class( $active_theme->Name ); ?><!--"-->
+                    <!--               type="checkbox" data='--><?php //echo json_encode( $active_theme ); ?><!--'-->
+                    <!--               value="--><?php //echo urlencode( json_encode( $active_theme ) ); ?><!--"><label-->
+                    <!--            for="--><?php //echo sanitize_html_class( $active_theme->Name ); ?><!--"><strong>--><?php //_e( 'Theme', 'redux-framework' ); ?>
+                    <!--                : </strong> --><?php //echo $active_theme->Name; ?>
+                    <!--            <small>v--><?php //echo $active_theme->Version; ?><!--</small>-->
+                    <!--        </label>-->
+                    <!--    </li>-->
+                    <!--</ul>-->
+                    <p>Should the developer not be responsive, read the following article before asking for support from us directly.<br />
+                        <a href="http://docs.reduxframework.com/core/support-defined/">http://docs.reduxframework.com/core/support-defined/</a>
                     </p>
 
 
