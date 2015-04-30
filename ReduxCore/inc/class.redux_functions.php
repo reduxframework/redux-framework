ee9674da68b89d1a2d97ea884e8118be43a5c22c
KProvance <kevin.provance@gmail.com>
2015-04-29 21:22:25 -0400

    #2274 Signed-off-by:KProvance <kevin.provance@gmail.com>

--- ReduxCore/inc/class.redux_functions.php
+++ ReduxCore/inc/class.redux_functions.php
@@ -35,6 +35,13 @@ if ( ! class_exists( 'Redux_Functions' ) ) {
             return $min;
         }
 
+
+        public static function setCookie( $name, $value, $expire = 0, $path, $domain = null, $secure = false, $httponly = false ){
+            if ( ! defined( 'WP_TESTS_DOMAIN' ) ) {
+                setcookie( $name, $value, $expire, $path, $domain, $secure, $httponly );
+            }
+        }        
+        
         /**
          * Parse CSS from output/compiler array
          *
