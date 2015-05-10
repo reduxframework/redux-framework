0f60667f16a9b8744875f9a13767cffad225e4e8
Aristeides Stathopoulos <aristath@gmail.com>
2015-05-10 19:15:42 +0300

    move lib-redux-instances.php to the includes folder.

--- ReduxCore/framework.php
+++ ReduxCore/framework.php
@@ -31,7 +31,7 @@ if ( has_action( 'ecpt_field_options_' ) ) {
     }
 }
 
-require_once( dirname( __FILE__ ) . '/inc/lib.redux_instances.php' );
+require_once( REDUX_PATH . '/includes/lib-redux-instances.php' );
 
 if ( class_exists( 'Redux_Framework_Instances' ) ) {
     add_action( 'redux/init', 'Redux_Framework_Instances::get_instance' );
