d35561c4a6366ca2cbc5419b50e245a93f9836da
Dovy <dovy@reduxframework.com>
2015-05-19 21:31:39 -0600

    Version update and changelog

--- class.redux-plugin.php
+++ class.redux-plugin.php
@@ -26,7 +26,7 @@
              * @since       3.0.0
              */
 
-            const VERSION = '3.5.4.10';
+            const VERSION = '3.5.4.11';
 
             /**
              * @access      protected
