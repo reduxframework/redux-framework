5da797f843d40e4c9075c4f37e81d387e51b9803
Dovy <dovy@redux.io>
2015-05-21 04:55:49 +0000

    SSL verify for fix for newsflash

--- ReduxCore/core/newsflash.php
+++ ReduxCore/core/newsflash.php
@@ -17,7 +17,7 @@
             public function __construct ($parent, $params) {
                 // set parent object
                 $this->parent = $parent;
-                
+
                 if ( !is_admin() ) {
                     return;
                 }
@@ -26,7 +26,7 @@
                 $this->server_file  = $server_file;
                 $this->interval     = isset($interval) ? $interval: 3;
                 $this->cookie_id    = isset($cookie_id) ? $cookie_id : $parent->args['opt_name'] . '_blast';
-                
+
                 // set notice file location
                 $notice_dir         = ReduxFramework::$_upload_dir . $dir_name;// 'notice';
                 $this->notice_json  = $notice_dir . '/notice.json';
@@ -54,21 +54,20 @@
             }
 
             private function get_notice_json() {
-
                 // filesystem object
                 $filesystem = $this->parent->filesystem;
-                
+
                 // get notice data from server
                 //$data = $filesystem->execute('get_contents', $this->server_file);// 'http://www.reduxframework.com/' . 'wp-content/uploads/redux/redux_notice.json');
+		add_filter( 'https_ssl_verify', '__return_false' );
                 $data = wp_remote_get($this->server_file);
-                $data = $data['body'];
-
+	        $data = $data['body'];
                 // if some data exists
                 if ($data != '' || !empty($data)) {
-                    
+
                     // if local notice file exists
                     if (file_exists($this->notice_json)) {
-                        
+
                         // get cached data
                         $cache_data = $filesystem->execute('get_contents', $this->notice_json);
 
@@ -76,12 +75,12 @@
                         if (  strcmp ( $data, $cache_data ) == 0) {
                             // set new cookie for interval value
                             Redux_Functions::setCookie( $this->cookie_id, time(), time() + (86400 * $this->interval), '/' );
-                            
+
                             // bail out
                             return;
                         }
                     }
-                
+
                     // set server data
                     $params = array(
                         'content' => $data
@@ -89,10 +88,10 @@
 
                     // write local notice file with new data
                     $filesystem->execute('put_contents', $this->notice_json, $params);
-                    
+
                     // set cookie for three day expiry
                     setcookie( $this->cookie_id, time(), time() + (86400 * $this->interval), '/' );
-                    
+
                     // set unique key for dismiss meta key
                     update_option($this->cookie_id, time());
                 }
@@ -103,13 +102,13 @@
                 if (file_exists($this->notice_json)) {
                     // get cached data
                     $data = $this->parent->filesystem->execute('get_contents', $this->notice_json);
-                    
+
                     // decode json string
                     $data = (Array)json_decode($data);
 
                     // must be array and not empty
                     if (is_array($data) && !empty($data)) {
-                        
+
                         // No message means nothing to display.
                         if (!isset($data['message']) || $data['message'] == '' || empty($data['message'])) {
                             return;
@@ -118,16 +117,16 @@
                         // validate data
                         $data['type']   = isset($data['type']) && $data['type'] != '' ? $data['type'] : 'updated';
                         $data['title']  = isset($data['title']) && $data['title'] != '' ? $data['title'] : '';
-                        
+
                         if ($data['type'] == 'redux-message') {
                             $data['type'] = 'updated redux-message';
                         }
-                        
+
                         $data['color']  = isset($data['color']) ? $data['color'] : '#00A2E3';
-                        
+
                         // get unique meta key
                         $key = get_option($this->cookie_id);
-                        
+
                         // set admin notice array
                         $this->parent->admin_notices[] = array(
                             'type'      => $data['type'],
