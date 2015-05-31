5a08aba4405f351dd852293f487f79a648b9137e
KProvance <kevin.provance@gmail.com>
2015-05-31 01:04:33 -0400

    #2351 Signed-off-by:KProvance <kevin.provance@gmail.com>

--- ReduxCore/framework.php
+++ ReduxCore/framework.php
@@ -25,7 +25,7 @@
     if ( has_action( 'ecpt_field_options_' ) ) {
         global $pagenow;
         if ( $pagenow === 'admin.php' ) {
-            /** @noinspection PhpUndefinedCallbackInspection */
+            
             remove_action( 'admin_init', 'pb_admin_init' );
         }
     }
