868812961bddf5faeaa5abd13f3997ee71b00742
KProvance <kevin.provance@gmail.com>
2015-08-17 16:48:09 -0400

    hide_save arg Signed-off-by:KProvance <kevin.provance@gmail.com>

--- ReduxCore/templates/panel/header_stickybar.tpl.php
+++ ReduxCore/templates/panel/header_stickybar.tpl.php
@@ -5,7 +5,7 @@
      *
      * @author        Redux Framework
      * @package       ReduxFramework/Templates
-     * @version:      3.5.5
+     * @version:      3.5.6.7
      */
 ?>
 <div id="redux-sticky">
@@ -16,7 +16,10 @@
 
         <div class="redux-action_bar">
             <span class="spinner"></span>
-            <?php submit_button( __( 'Save Changes', 'redux-framework' ), 'primary', 'redux_save', false ); ?>
+            <?php if ( false === $this->parent->args['hide_save'] ) : ?>
+                <?php submit_button( __( 'Save Changes', 'redux-framework' ), 'primary', 'redux_save', false ); ?>
+            <?php endif; ?>
+            
             <?php if ( false === $this->parent->args['hide_reset'] ) : ?>
                 <?php submit_button( __( 'Reset Section', 'redux-framework' ), 'secondary', $this->parent->args['opt_name'] . '[defaults-section]', false, array( 'id' => 'redux-defaults-section' ) ); ?>
                 <?php submit_button( __( 'Reset All', 'redux-framework' ), 'secondary', $this->parent->args['opt_name'] . '[defaults]', false, array( 'id' => 'redux-defaults' ) ); ?>
