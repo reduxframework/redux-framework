99a930a4c4ae6e2b18cd917b46ed58b9c0cb12c8
Dovy Paukstys <info@simplerain.com>
2016-04-17 00:16:47 -0600

    Bah, back out a PR that broke things for windows users.

--- ReduxCore/inc/extensions/customizer/extension_customizer.php
+++ ReduxCore/inc/extensions/customizer/extension_customizer.php
@@ -81,8 +81,8 @@
                 }
 
                 if ( empty( $this->_extension_dir ) ) {
-                    $this->_extension_dir = Redux_Helpers::get_extension_dir( __FILE__ );
-                    $this->_extension_url = str_replace( wp_normalize_path( WP_CONTENT_DIR ), WP_CONTENT_URL, $this->_extension_dir );
+                    $this->_extension_dir = trailingslashit( str_replace( '\\', '/', dirname( __FILE__ ) ) );
+                    $this->_extension_url = site_url( str_replace( trailingslashit( str_replace( '\\', '/', ABSPATH ) ), '', $this->_extension_dir ) );
                 }
 
                 self::get_post_values();
