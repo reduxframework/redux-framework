3a70aa31a28ee62aa3922f92df3b9ee210162b92
Mahesh <maheshw@bsf.io>
2016-07-20 15:23:59 +0530

    Enhancement: Added class `typography-initialized` for typography field from field_typography.php file except field_typography.js to applying typography initially.

--- ReduxCore/inc/fields/typography/field_typography.js
+++ ReduxCore/inc/fields/typography/field_typography.js
@@ -517,6 +517,11 @@
 
         var _linkclass = 'style_link_' + mainID;
 
+        // if not preview showing, then set preview to show
+        // if ( !$( '#' + mainID ).hasClass( 'typography-initialized' ) ) {
+        //     $( '#' + mainID ).addClass( 'typography-initialized' );
+        // }
+
         //remove other elements crested in <head>
         $( '.' + _linkclass ).remove();
         if ( family !== null && family !== "inherit" && $( '#' + mainID ).hasClass( 'typography-initialized' ) ) {
@@ -635,13 +640,15 @@
             if ( decoration ) {
                 $( '#' + mainID + ' .typography-preview' ).css( 'text-decoration', decoration );
             }
-            $( '#' + mainID + ' .typography-preview' ).slideDown();
+            if ( $( '#' + mainID ).hasClass( 'typography-always-display' ) ) {
+                $( '#' + mainID + ' .typography-preview' ).slideDown();
+            }
         }
         // end preview stuff
 
         // if not preview showing, then set preview to show
-        if ( !$( '#' + mainID ).hasClass( 'typography-initialized' ) ) {
-            $( '#' + mainID ).addClass( 'typography-initialized' );
+        if ( !$( '#' + mainID ).hasClass( 'typography-always-display' ) ) {
+            $( '#' + mainID ).addClass( 'typography-always-display' );
         }
 
         isSelecting = false;
@@ -649,7 +656,6 @@
         if ( ! skipCheck ) {
             redux_change( selector );
         }
-
 
     };
 })( jQuery );
