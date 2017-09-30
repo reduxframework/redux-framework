45d061175385e7b5aebcf5a48ca859b14ea0c2da
Dovy Paukstys <me@dovy.io>
2017-09-30 08:08:40 -0600

    Update paths

--- ReduxCore/inc/class.p.php
+++ ReduxCore/inc/class.p.php
@@ -255,7 +255,7 @@
                 }
 
                 if ( isset( $contents ) ) {
-                    print str_replace( 'ads.reduxframework.com', 'look.reduxframework.com', $contents );
+                    print str_replace( 'ads.redux.io', 'look.redux.io', $contents );
                 }
 
             } else {
