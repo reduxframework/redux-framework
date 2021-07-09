81dbbaf64b72e567a97a79154753f41dd00ecb05
Kevin Provance <kevin.provance@gmail.com>
2021-07-08 21:54:58 -0400

    Pro no longer required for automatic google font updates.
    Signed-off-by: Kevin Provance <kevin.provance@gmail.com>

--- redux-core/inc/fields/typography/class-redux-typography.php
+++ redux-core/inc/fields/typography/class-redux-typography.php
@@ -740,11 +740,7 @@ if ( ! class_exists( 'Redux_Typography', false ) ) {
 						// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 						echo '<small class="description" style="display:none;"><br />' . Redux_Connection_Banner::tos_blurb( 'google_fonts' ) . ' </small>';
 					} else {
-						if ( ! Redux_Helpers::mokama() ) {
-							echo '&nbsp;' . esc_html__( 'Run a', 'redux-framework' ) . ' <a href="#" class="update-google-fonts" data-action="manual" aria-label="' . esc_attr__( 'one-time update', 'redux-framework' ) . '">' . esc_html__( 'one-time update', 'redux-framework' ) . '</a>.';
-						} else {
-							echo '&nbsp;<a href="#" class="update-google-fonts" data-action="automatic" aria-label="' . esc_attr__( 'Automated updates', 'redux-framework' ) . '">' . esc_html__( 'Automated updates', 'redux-framework' ) . '</a> or <a href="#" class="update-google-fonts" data-action="manual" aria-label="' . esc_attr__( 'one-time update', 'redux-framework' ) . '">' . esc_html__( 'one-time update', 'redux-framework' ) . '</a>.';
-						}
+						echo '&nbsp;<a href="#" class="update-google-fonts" data-action="automatic" aria-label="' . esc_attr__( 'Automated updates', 'redux-framework' ) . '">' . esc_html__( 'Automated updates', 'redux-framework' ) . '</a> or <a href="#" class="update-google-fonts" data-action="manual" aria-label="' . esc_attr__( 'one-time update', 'redux-framework' ) . '">' . esc_html__( 'one-time update', 'redux-framework' ) . '</a>.';
 					}
 
 					echo '</p>';
