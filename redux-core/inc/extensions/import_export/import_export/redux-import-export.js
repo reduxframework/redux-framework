5dc2f29ba6e44357ce35e023a0984c7da92122d3
Kevin Provance <kevin.provance@gmail.com>
2021-08-03 22:51:18 -0400

    jQuery deprecation fixes.
    Signed-off-by: Kevin Provance <kevin.provance@gmail.com>

--- redux-core/inc/extensions/import_export/import_export/redux-import-export.js
+++ redux-core/inc/extensions/import_export/import_export/redux-import-export.js
@@ -1,9 +1,9 @@
-/* global jQuery, document, redux, ajaxurl */
+/* global jQuery, document, redux, ajaxurl, ImportExport */
 
 (function( $ ) {
 	'use strict';
 
-	redux.field_objects = redux.field_objects || {};
+	redux.field_objects               = redux.field_objects || {};
 	redux.field_objects.import_export = redux.field_objects.import_export || {};
 
 	redux.field_objects.import_export.copy_text = function( $text ) {
@@ -18,16 +18,24 @@
 	redux.field_objects.import_export.get_options = function( $secret ) {
 		var $el = $( '#redux-export-code-copy' );
 		var url = ajaxurl + '?download=0&action=redux_download_options-' + redux.optName.args.opt_name + '&secret=' + $secret;
+
 		$el.addClass( 'disabled' ).attr( 'disabled', 'disabled' );
 		$el.text( $el.data( 'copy' ) );
-		$.get( url, function( data ) {
-			redux.field_objects.import_export.copy_text( data );
-			$el.removeClass( 'disabled' );
-			$el.text( $el.data( 'copied' ) );
-			setTimeout( function() {
-				$el.text( $el.data( 'copy' ) ).removeClass( 'disabled' ).removeAttr( 'disabled' );
-			}, 2000 );
-		} );
+
+		$.get(
+			url,
+			function( data ) {
+				redux.field_objects.import_export.copy_text( data );
+				$el.removeClass( 'disabled' );
+				$el.text( $el.data( 'copied' ) );
+				setTimeout(
+					function() {
+						$el.text( $el.data( 'copy' ) ).removeClass( 'disabled' ).removeAttr( 'disabled' );
+					},
+					2000
+				);
+			}
+		);
 	};
 
 	redux.field_objects.import_export.init = function( selector ) {
@@ -38,7 +46,7 @@
 				var textBox1;
 				var textBox2;
 
-				var el = $( this );
+				var el     = $( this );
 				var parent = el;
 
 				if ( ! el.hasClass( 'redux-field-container' ) ) {
@@ -59,8 +67,7 @@
 					function() {
 						$( '#redux-import' ).click(
 							function( e ) {
-								if ( '' === $( '#import-code-value' ).val() && '' === $(
-									'#import-link-value' ).val() ) {
+								if ( '' === $( '#import-code-value' ).val() && '' === $( '#import-link-value' ).val() ) {
 									e.preventDefault();
 									return false;
 								}
@@ -98,87 +105,79 @@
 							}
 						);
 
-						$( this ).find( '#redux-import-link-button' ).click(
-							function() {
-								var $el = $( '#redux-import-link-wrapper' );
-								if ( $( '#redux-import-code-wrapper' ).is( ':visible' ) ) {
-									$( '#import-code-value' ).text( '' );
-									$( '#redux-import-code-wrapper' ).fadeOut(
-										'fast',
-										function() {
-											$el.fadeIn(
-												'fast',
-												function() {
-													$( '#import-link-value' ).focus();
-												}
-											);
-										}
-									);
-								} else {
-									if ( $el.is( ':visible' ) ) {
-										$el.fadeOut();
-									} else {
-										$el.fadeIn(
-											'medium',
+						$( this ).find( '#redux-export-code-dl' ).on(
+							'click',
+							function( e ) {
+								e.preventDefault();
+
+								if ( !! window.onbeforeunload ) {
+									if ( confirm( ImportExport.unchanged_values ) ) {
+										$( '#redux_top_save' ).click();
+										setTimeout(
 											function() {
-												$( '#import-link-value' ).focus();
-											}
+												window.open( $( this ).attr( 'href' ) );
+											},
+											2000
 										);
 									}
+								} else {
+									window.open( $( this ).attr( 'href' ) );
 								}
 							}
 						);
-						$( this ).find( '#redux-export-code-dl' ).click( function( e ) {
-							e.preventDefault();
 
-							if ( !! window.onbeforeunload ) {
-								if ( confirm( 'Your panel has unchanged values, would you like to save them now?' ) ) {
-									$( '#redux_top_save' ).click();
-									setTimeout( function() {
-										window.open( $( this ).attr( 'href' ) );
-									}, 2000 );
-								}
-							} else {
-								window.open( $( this ).attr( 'href' ) );
+						$( this ).find( '#redux-import-upload' ).on(
+							'click',
+							function() {
+								$( '#redux-import-upload-file' ).click();
 							}
-						} );
-						$( this ).find( '#redux-import-upload' ).click( function() {
-							$( '#redux-import-upload-file' ).click();
-						} );
+						);
 
-						document.getElementById( 'redux-import-upload-file' ).addEventListener( 'change', function() {
-							var file_to_read = document.getElementById( 'redux-import-upload-file' ).files[0];
-							var fileread = new FileReader();
-							$( '#redux-import-upload span' ).text( ': ' + file_to_read.name );
-							fileread.onload = function() {
-								var content = fileread.result;
-								$( '#import-code-value' ).val( content );
-							};
-							fileread.readAsText( file_to_read );
-						} );
-						$( this ).find( '#redux-export-code-copy' ).click(
+						document.getElementById( 'redux-import-upload-file' ).addEventListener(
+							'change',
+							function() {
+								var file_to_read = document.getElementById( 'redux-import-upload-file' ).files[0];
+								var fileread     = new FileReader();
+
+								$( '#redux-import-upload span' ).text( ': ' + file_to_read.name );
+
+								fileread.onload = function() {
+									var content = fileread.result;
+									$( '#import-code-value' ).val( content );
+								};
+
+								fileread.readAsText( file_to_read );
+							}
+						);
+
+						$( this ).find( '#redux-export-code-copy' ).on(
+							'click',
 							function( e ) {
-								var $el = $( '#redux-export-code' );
+								var $el     = $( '#redux-export-code' );
 								var $secret = $( this ).data( 'secret' );
+
 								e.preventDefault();
 								if ( !! window.onbeforeunload ) {
-									if ( confirm(
-										'Your panel has unchanged values, would you like to save them now?' ) ) {
+									if ( confirm( ImportExport.unchanged_values ) ) {
 										$( '#redux_top_save' ).click();
-										setTimeout( function() {
-											redux.field_objects.import_export.get_options( $secret, $el );
-										}, 2000 );
+										setTimeout(
+											function() {
+												redux.field_objects.import_export.get_options( $secret, $el );
+											},
+											2000
+										);
 									}
 								} else {
 									redux.field_objects.import_export.get_options( $secret, $el );
 								}
 							}
 						);
+
 						$( this ).find( 'textarea' ).on(
 							'focusout',
 							function() {
-								var $id = $( this ).attr( 'id' );
-								var $el = $( this );
+								var $id        = $( this ).attr( 'id' );
+								var $el        = $( this );
 								var $container = $el;
 
 								if ( 'import-link-value' === $id || 'import-code-value' === $id ) {
@@ -193,20 +192,6 @@
 										}
 									}
 								);
-							}
-						);
-
-						$( this ).find( '#redux-export-link' ).click(
-							function() {
-								var $el = $( this );
-								$el.addClass( 'disabled' ).attr( 'disabled', 'disabled' );
-								$el.text( $el.data( 'copy' ) );
-								redux.field_objects.import_export.copy_text( $el.data( 'url' ) );
-								$el.removeClass( 'disabled' );
-								$el.text( $el.data( 'copied' ) );
-								setTimeout( function() {
-									$el.text( $el.data( 'copy' ) ).removeClass( 'disabled' ).removeAttr( 'disabled' );
-								}, 2000 );
 							}
 						);
 
