7679a615c22f4ab1ae966e32b239caf9a745ec88
Kevin Provance <kevin.provance@gmail.com>
2021-11-30 12:37:30 -0500

    Add search extension
    Signed-off-by: Kevin Provance <kevin.provance@gmail.com>

---
+++ redux-core/inc/extensions/search/class-redux-extension-search.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Redux Search Extension Class
+ *
+ * @package Redux
+ * @author  Dovy Paukstys (dovy)
+ * @class   Redux_Extension_Search
+ * @version 3.4.5
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'Redux_Extension_Search' ) ) {
+
+	/**
+	 * Class Redux_Extension_Search
+	 */
+	class Redux_Extension_Search extends Redux_Extension_Abstract {
+
+		/**
+		 * Extension version.
+		 *
+		 * @var string
+		 */
+		public static $version = '3.4.5';
+
+		/**
+		 * Extension friendly name.
+		 *
+		 * @var string
+		 */
+		public $extension_name = 'Search';
+
+		/**
+		 * Redux_Extension_Search constructor.
+		 *
+		 * @param object $parent ReduxFramework object pointer.
+		 */
+		public function __construct( $parent ) {
+			global $pagenow;
+
+			if ( false === $parent->args['search'] ) {
+				return;
+			}
+
+			parent::__construct( $parent, __FILE__ );
+
+			$this->add_field( 'search' );
+
+			// Allow users to extend if they want.
+			// phpcs:ignore WordPress.NamingConventions.ValidHookName
+			do_action( 'redux/search/' . $parent->args['opt_name'] . '/construct' );
+
+			if ( isset( $_GET['page'] ) && sanitize_text_field( wp_unslash( $_GET['page'] === $this->parent->args['page_slug'] ) ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+				add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ), 0 );
+			}
+
+			add_action( "redux/metaboxes/{$this->parent->args[ 'opt_name' ]}/enqueue", array( $this, 'enqueue' ), 10 );
+		}
+
+		/**
+		 * Support file enqueue.
+		 */
+		public function enqueue() {
+			$min = Redux_Functions::is_min();
+
+			/**
+			 * Redux search CSS
+			 * filter 'redux/page/{opt_name}/enqueue/redux-extension-search-css'
+			 *
+			 * @param string  bundled stylesheet src
+			 */
+			wp_enqueue_style(
+				'redux-extension-search-css',
+				// phpcs:ignore WordPress.NamingConventions.ValidHookName
+				apply_filters( "redux/search/{$this->parent->args[ 'opt_name' ]}/enqueue/redux-extension-search-css", $this->extension_url . 'redux-extension-search.css' ),
+				array(),
+				self::$version
+			);
+
+			/**
+			 * Redux search JS
+			 * filter 'redux/page/{opt_name}/enqueue/redux-extension-search-js
+			 *
+			 * @param string  bundled javscript
+			 */
+			wp_enqueue_script(
+				'redux-extension-search-js',
+				// phpcs:ignore WordPress.NamingConventions.ValidHookName
+				apply_filters( "redux/search/{$this->parent->args[ 'opt_name' ]}/enqueue/redux-extension-search-js", $this->extension_url . 'redux-extension-search' . $min . '.js' ),
+				'',
+				self::$version,
+				true
+			);
+
+			// Values used by the javascript.
+			wp_localize_script(
+				'redux-extension-search-js',
+				'reduxsearch',
+				array(
+					'search' => esc_html__( 'Search for field(s)', 'redux-pro' ),
+				)
+			);
+		}
+	}
+}
---
+++ redux-core/inc/extensions/search/redux-extension-search.js
@@ -0,0 +1,96 @@
+/* global jQuery, reduxsearch */
+
+(function( $ ) {
+	$( document ).ready(
+		function() {
+			$( '.redux-container' ).each(
+				function( ) {
+					if ( ! $( this ).hasClass( 'redux-no-sections' ) ) {
+						$( this ).find( '.redux-main' ).prepend( '<input class="redux_field_search" type="text" placeholder="' + reduxsearch.search + '"/>' );
+					}
+				}
+			);
+
+			$( '.redux_field_search' ).on(
+				'keypress',
+				function( evt ) {
+
+					// Deterime where our character code is coming from within the event.
+					var charCode = evt.charCode || evt.keyCode;
+
+					if ( 13 === charCode ) { // Enter key's keycode.
+						return false;
+					}
+				}
+			);
+
+			$( '.redux_field_search' ).typeWatch(
+				{
+					callback: function( searchString ) {
+						var searchArray;
+						var parent;
+						var expanded_options;
+
+						searchString = searchString.toLowerCase();
+
+						searchArray = searchString.split( ' ' );
+						parent      = $( this ).parents( '.redux-container:first' );
+
+						expanded_options = parent.find( '.expand_options' );
+
+						if ( '' !== searchString ) {
+							if ( ! expanded_options.hasClass( 'expanded' ) ) {
+								expanded_options.trigger( 'click' );
+								parent.find( '.redux-main' ).addClass( 'redux-search' );
+							}
+						} else {
+							if ( expanded_options.hasClass( 'expanded' ) ) {
+								expanded_options.trigger( 'click' );
+								parent.find( '.redux-main' ).removeClass( 'redux-search' );
+							}
+							parent.find( '.redux-section-field, .redux-info-field, .redux-notice-field, .redux-container-group, .redux-section-desc, .redux-group-tab h3' ).show();
+						}
+
+						parent.find( '.redux-field-container' ).each(
+							function() {
+								if ( '' !== searchString ) {
+									$( this ).parents( 'tr:first' ).hide();
+								} else {
+									$( this ).parents( 'tr:first' ).show();
+								}
+							}
+						);
+
+						parent.find( '.form-table tr' ).filter(
+							function() {
+								var isMatch = true, text = $( this ).find( '.redux_field_th' ).text().toLowerCase();
+
+								if ( ! text || '' === text ) {
+									return false;
+								}
+
+								$.each(
+									searchArray,
+									function( i, searchStr ) {
+										if ( -1 === text.indexOf( searchStr ) ) {
+											isMatch = false;
+										}
+									}
+								);
+
+								if ( isMatch ) {
+									$( this ).show();
+								}
+
+								return isMatch;
+							}
+						).show();
+					},
+					wait: 400,
+					highlight: false,
+					captureLength: 0
+				}
+			);
+		}
+	);
+} )( jQuery );
