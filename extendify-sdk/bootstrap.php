343581bcdea01018d500f216d28fb71b954574d3
Kev Provance <kevin.provance@gmail.com>
2022-04-27 19:49:49 -0400

    Extendify partner detect improvements.
    Signed-off-by: Kev Provance <kevin.provance@gmail.com>

--- extendify-sdk/bootstrap.php
+++ extendify-sdk/bootstrap.php
@@ -3,44 +3,46 @@
  * Bootstrap the application
  */
 
+use Extendify\Library\App;
 use Extendify\Library\Admin;
-use Extendify\Library\Frontend;
 use Extendify\Library\Shared;
 use Extendify\Library\Welcome;
+use Extendify\Library\Frontend;
 
 if (!defined('ABSPATH')) {
-    die('No direct access.');
+	die('No direct access.');
 }
 
 if (!defined('EXTENDIFY_PATH')) {
-    define('EXTENDIFY_PATH', \plugin_dir_path(__FILE__));
+	define('EXTENDIFY_PATH', \plugin_dir_path(__FILE__));
 }
 
 if (!defined('EXTENDIFY_URL')) {
-    define('EXTENDIFY_URL', \plugin_dir_url(__FILE__));
+	define('EXTENDIFY_URL', \plugin_dir_url(__FILE__));
 }
 
 if (!defined('EXTENDIFY_PLUGIN_BASENAME')) {
-    define('EXTENDIFY_PLUGIN_BASENAME', \plugin_basename(__DIR__ . '/extendify.php'));
+	define('EXTENDIFY_PLUGIN_BASENAME', \plugin_basename(__DIR__ . '/extendify.php'));
 }
 
 if (is_readable(EXTENDIFY_PATH . 'vendor/autoload.php')) {
-    require EXTENDIFY_PATH . 'vendor/autoload.php';
+	require EXTENDIFY_PATH . 'vendor/autoload.php';
 }
 
-$extendifyAdmin = new Admin();
-$extendifyFrontend = new Frontend();
-$extendifyShared = new Shared();
-$extendifyWelcome = new Welcome();
+new App();
+new Admin();
+new Frontend();
+new Shared();
+new Welcome();
 
 require EXTENDIFY_PATH . 'routes/api.php';
 require EXTENDIFY_PATH . 'editorplus/EditorPlus.php';
 
 \add_action(
-    'init',
-    function () {
-        \load_plugin_textdomain('extendify', false, EXTENDIFY_PATH . 'languages');
-    }
+	'init',
+	function () {
+		\load_plugin_textdomain('extendify', false, EXTENDIFY_PATH . 'languages');
+	}
 );
 
 // To cover legacy conflicts.
--- extendify-sdk/extendify.php
+++ extendify-sdk/extendify.php
@@ -1,2 +1,74 @@
 <?php
- if (!defined('ABSPATH')) { exit; } if (!class_exists('ExtendifySdk') && !class_exists('Extendify')) : final class Extendify { public static $loaded = false; public function __invoke() { if (!apply_filters('extendify_load_library', true) || !apply_filters('extendifysdk_load_library', true)) { return; } if (version_compare(PHP_VERSION, '5.6', '<') || version_compare($GLOBALS['wp_version'], '5.5', '<')) { return; } if (!self::$loaded) { self::$loaded = true; require dirname(__FILE__) . '/bootstrap.php'; $app = new Extendify\Library\App(); if (!defined('EXTENDIFY_BASE_URL')) { define('EXTENDIFY_BASE_URL', plugin_dir_url(__FILE__)); } } } } $extendify = new Extendify(); $extendify(); endif; 
+/**
+ * Plugin Name:       Extendify
+ * Description:       Extendify is the platform of site design and creation tools for people that want to build a beautiful WordPress website with a library of patterns and full page layouts for the Gutenberg block editor.
+ * Plugin URI:        https://extendify.com/?utm_source=wp-plugins&utm_campaign=plugin-uri&utm_medium=wp-dash
+ * Author:            Extendify
+ * Author URI:        https://extendify.com/?utm_source=wp-plugins&utm_campaign=author-uri&utm_medium=wp-dash
+ * Version:           0.8.0
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       extendify
+ *
+ * Extendify is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Extendify is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+/** ExtendifySdk is the previous class name used */
+if (!class_exists('ExtendifySdk') && !class_exists('Extendify')) :
+
+	/**
+	 * The Extendify Library
+	 */
+	// phpcs:ignore Squiz.Classes.ClassFileName.NoMatch,Squiz.Commenting.ClassComment.Missing,PEAR.Commenting.ClassComment.Missing
+	final class Extendify
+	{
+
+		/**
+		 * Var to make sure we only load once
+		 *
+		 * @var boolean $loaded
+		 */
+		public static $loaded = false;
+
+		/**
+		 * Set up the Library
+		 *
+		 * @return void
+		 */
+		public function __invoke()
+		{
+			// Allow users to disable the libary. The latter is left in for historical reasons.
+			if (!apply_filters('extendify_load_library', true) || !apply_filters('extendifysdk_load_library', true)) {
+				return;
+			}
+
+			if (version_compare(PHP_VERSION, '5.6', '<') || version_compare($GLOBALS['wp_version'], '5.5', '<')) {
+				return;
+			}
+
+			if (!self::$loaded) {
+				self::$loaded = true;
+				require dirname(__FILE__) . '/bootstrap.php';
+				if (!defined('EXTENDIFY_BASE_URL')) {
+					define('EXTENDIFY_BASE_URL', plugin_dir_url(__FILE__));
+				}
+			}
+		}
+		// phpcs:ignore Squiz.Classes.ClassDeclaration.SpaceBeforeCloseBrace
+	}
+
+	$extendify = new Extendify();
+	$extendify();
+endif;
