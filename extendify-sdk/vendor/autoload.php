bb0f421c805ac3ac96f3a900586d81d49f3c32cc
kprovance <kprovance@users.noreply.github.com>
2022-10-08 07:34:58 +0000

    Update Extendify Library

--- extendify-sdk/vendor/autoload.php
+++ extendify-sdk/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit8544c08622507563b6e7bc7776a1f8d3::getLoader();
+return ComposerAutoloaderInitfefed663b8bfec545285769e014a1afb::getLoader();
