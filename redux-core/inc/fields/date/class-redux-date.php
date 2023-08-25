42f31c1cbed0c933ae8348729d0a49c511a01432
Kev Provance <kevin.provance@gmail.com>
2023-08-24 22:53:00 -0400

    Revert date field to the original way we did it.

--- redux-core/inc/fields/date/class-redux-date.php
+++ redux-core/inc/fields/date/class-redux-date.php
@@ -5,6 +5,7 @@
  * @package     ReduxFramework/Fields
  * @author      Dovy Paukstys & Kevin Provance (kprovance)
  * @version     4.0.0
+ * @noinspection PhpIgnoredClassAliasDeclaration
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -28,14 +29,16 @@ if ( ! class_exists( 'Redux_Date', false ) ) {
 		 * @return        void
 		 */
 		public function render() {
-			$this->field['attributes']['data-id']     = $this->field['id'];
-			$this->field['attributes']['id']          = $this->field['id'];
-			$this->field['attributes']['name']        = esc_attr( $this->field['name'] . $this->field['name_suffix'] );
-			$this->field['attributes']['value']       = $this->value;
-			$this->field['attributes']['placeholder'] = ( isset( $this->field['placeholder'] ) && ! is_array( $this->field['placeholder'] ) ) ? esc_attr( $this->field['placeholder'] ) : '';
-			$this->field['attributes']['class']       = 'redux-datepicker regular-text ' . esc_attr( $this->field['class'] );
-			$attributes_string                        = $this->render_attributes( $this->field['attributes'] );
-			echo '<input ' . $attributes_string . '>'; // phpcs:ignore WordPress.Security.EscapeOutput
+			$placeholder = $this->field['placeholder'] ?? '';
+
+			echo '<input
+					data-id="' . esc_attr( $this->field['id'] ) . '"
+					type="text"
+					id="' . esc_attr( $this->field['id'] ) . '-date"
+					name="' . esc_attr( $this->field['name'] . $this->field['name_suffix'] ) . '"
+					value="' . esc_attr( $this->value ) . '"
+					placeholder="' . esc_attr( $placeholder ) . '"
+					class="redux-datepicker regular-text ' . esc_attr( $this->field['class'] ) . '" />';
 		}
 
 		/**
