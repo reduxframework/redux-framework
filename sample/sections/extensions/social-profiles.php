255b0ee1cce8c2ad1910f8b9d3ca0bf3619b3f63
Kev Provance <kevin.provance@gmail.com>
2024-05-02 17:39:24 -0400

    `social_profiles` in customizer fixed.

--- sample/sections/extensions/social-profiles.php
+++ sample/sections/extensions/social-profiles.php
@@ -11,11 +11,12 @@ defined( 'ABSPATH' ) || exit;
 Redux::set_section(
 	$opt_name,
 	array(
-		'title'      => esc_html__( 'Social Profiles', 'your-textdomain-here' ),
-		'desc'       => esc_html__( 'For full documentation on this field, visit: ', 'your-textdomain-here' ) . '<a href="https://devs.redux.io/core-extensions/social-profiles.html" target="_blank">https://devs.redux.io/core-extensions/social-profiles.html</a>',
-		'subtitle'   => esc_html__( 'Click an icon to activate it, drag and drop to change the icon order.', 'your-textdomain-here' ),
-		'subsection' => true,
-		'fields'     => array(
+		'title'            => esc_html__( 'Social Profiles', 'your-textdomain-here' ),
+		'desc'             => esc_html__( 'For full documentation on this field, visit: ', 'your-textdomain-here' ) . '<a href="https://devs.redux.io/core-extensions/social-profiles.html" target="_blank">https://devs.redux.io/core-extensions/social-profiles.html</a>',
+		'subtitle'         => esc_html__( 'Click an icon to activate it, drag and drop to change the icon order.', 'your-textdomain-here' ),
+		'subsection'       => true,
+		'customizer_width' => '350px',
+		'fields'           => array(
 			array(
 				'id'              => 'opt-social-profiles',
 				'type'            => 'social_profiles',
