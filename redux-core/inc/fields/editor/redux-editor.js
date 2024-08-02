8fe3c17d6775afe93cd6788650cfc265e0c54b4f
Kev Provance <kevin.provance@gmail.com>
2024-08-02 00:59:34 -0400

    #4011 - `editor` fields in repeater not saving.

--- redux-core/inc/fields/editor/redux-editor.js
+++ redux-core/inc/fields/editor/redux-editor.js
@@ -8,18 +8,54 @@
 
 /*global redux_change, tinymce, redux*/
 
-(function( $ ) {
+(function ( $ ) {
 	'use strict';
 
 	redux.field_objects        = redux.field_objects || {};
 	redux.field_objects.editor = redux.field_objects.editor || {};
 
-	redux.field_objects.editor.init = function() {
+	redux.field_objects.editor.init = function ( selector ) {
 		var i;
 		var len;
 
+		selector = $.redux.getSelector( selector, 'editor' );
+
+		$( selector ).each(
+			function () {
+				var el     = $( this );
+				var parent = el;
+				var id;
+				var mce;
+				var editorArea;
+
+				if ( ! el.hasClass( 'redux-field-container' ) ) {
+					parent = el.parents( '.redux-field-container:first' );
+				}
+
+				if ( parent.is( ':hidden' ) ) {
+					return;
+				}
+
+				if ( parent.hasClass( 'redux-field-init' ) ) {
+					parent.removeClass( 'redux-field-init' );
+				} else {
+					return;
+				}
+
+				// Editor in repeater needs to have content manually added to text area,
+				// or the content won't save.
+				editorArea = el.find( '.wp-editor-area' );
+				if ( editorArea.hasClass( 'in-repeater' ) || editorArea.hasClass( 'in-tabbed' ) ) {
+					id  = el.data( 'id' );
+					mce = tinymce.editors[id];
+
+					redux.field_objects.editor.onKeyup( el, mce );
+				}
+			}
+		);
+
 		setTimeout(
-			function() {
+			function () {
 				if ( 'undefined' !== typeof ( tinymce ) ) {
 					len = tinymce.editors.length;
 
@@ -32,12 +68,24 @@
 		);
 	};
 
-	redux.field_objects.editor.onChange = function( i ) {
+	redux.field_objects.editor.onKeyup = function ( el, mce ) {
+		mce.on(
+			'keyup',
+			function () {
+				var id      = el.data( 'id' );
+				var content = tinymce.get( id ).getContent();
+
+				el.find( '.wp-editor-area' ).text( content );
+
+			}
+		);
+	};
+
+	redux.field_objects.editor.onChange = function ( i ) {
 		tinymce.editors[i].on(
 			'change',
-			function( e ) {
-				var el = jQuery( e.target.contentAreaContainer );
-				if ( 0 !== el.parents( '.redux-container-editor:first' ).length ) {
+			function () {
+				if ( 0 !== $( '.wp-editor-area', window.parent.document ) ) {
 					redux_change( $( '.wp-editor-area' ) );
 				}
 			}
