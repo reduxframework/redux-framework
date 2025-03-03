4c0cea08e878e64ad769975246c8ba1e9abfa4d9
Kev Provance <kevin.provance@gmail.com>
2025-03-03 18:07:15 -0500

    Prevent error on failed demo installs
    Signed-off-by: Kev Provance <kevin.provance@gmail.com>

--- redux-core/inc/fields/gallery/class-redux-gallery.php
+++ redux-core/inc/fields/gallery/class-redux-gallery.php
@@ -35,12 +35,14 @@ if ( ! class_exists( 'Redux_Gallery', false ) ) {
 
 				foreach ( $ids as $attachment_id ) {
 					$img = wp_get_attachment_image_src( $attachment_id );
-					$alt = wp_prepare_attachment_for_js( $attachment_id );
-					$alt = $alt['alt'] ?? '';
+					if ( ! false === $img ) {
+						$alt = wp_prepare_attachment_for_js( $attachment_id );
+						$alt = $alt['alt'] ?? '';
 
-					echo '<a class="of-uploaded-image" href="' . esc_url( $img[0] ) . '">';
-					echo '<img class="redux-option-image" id="image_' . esc_attr( $this->field['id'] ) . '_' . esc_attr( $attachment_id ) . '" src="' . esc_url( $img[0] ) . '" alt="' . esc_attr( $alt ) . '" target="_blank" rel="external" />';
-					echo '</a>';
+						echo '<a class="of-uploaded-image" href="' . esc_url( $img[0] ) . '">';
+						echo '<img class="redux-option-image" id="image_' . esc_attr( $this->field['id'] ) . '_' . esc_attr( $attachment_id ) . '" src="' . esc_url( $img[0] ) . '" alt="' . esc_attr( $alt ) . '" target="_blank" rel="external" />';
+						echo '</a>';
+					}
 				}
 			}
 
